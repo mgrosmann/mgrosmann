2194954d73552c0b4b0e6848397783b0e3844cc0
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-19 10:10:53 +0100

    Update caca.sh

--- script/caca.sh
+++ script/caca.sh
@@ -2,5 +2,5 @@ rm nextcloud.sh
 rm nextcloud.sh.1
 rm nextcloud.sh.2
 rm nextcloud.sh.3
-wget http://sio.jbdelasalle.com/~mgrosmann/nextcloud.sh
+wget https://mgrosmann.vercel.app/script/nextcloud.sh
 chmod +x nextcloud.sh
