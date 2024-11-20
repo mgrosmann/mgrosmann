e24512e319cc63d34748d2a39f6ad0a32d6d52f2
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-20 13:46:14 +0100

    Create setup.sh

---
+++ script/parrot/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+wget https://mgrosmann.vercel.app/script/docker.sh
+wget https://mgrosmann.vercel.app/script/edit.sh
+wget https://mgrosmann.vercel.app/script/glpi.sh
+wget https://mgrosmann.vercel.app/script/nextcloud.sh
+wget https://mgrosmann.vercel.app/script/promotheus.sh
+wget https://mgrosmann.vercel.app/script/remove.sh
+chmod +x docker.sh
+chmod +x edit.sh
+chmod +x glpi.sh
+chmod +x nextcloud.sh
+chmod +x promotheus.sh
+chmod +x remove.sh
+bash docker.sh
+bash glpi.sh
+bash promotheus.sh
+bash edit.sh nextcloud.sh nc.sh
+bash nc.sh
