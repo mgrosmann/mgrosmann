bec04aa78b01c3a398fda069fb9f969ab17e1cbe
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-20 18:34:20 +0100

    Update debian.sh

--- script/debian.sh
+++ script/debian.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-wget https://mgrosmann.vercel.app/script/deb_docker.sh
-wget https://mgrosmann.vercel.app/script/edit.sh
+wget https://mgrosmann.vercel.app/script/docker.sh
 wget https://mgrosmann.vercel.app/script/glpi.sh
 wget https://mgrosmann.vercel.app/script/nextcloud.sh
 wget https://mgrosmann.vercel.app/script/promotheus.sh
 wget https://mgrosmann.vercel.app/script/remove.sh
-chmod +x deb_docker.sh
-chmod +x edit.sh
+chmod +x docker.sh
 chmod +x glpi.sh
 chmod +x nextcloud.sh
 chmod +x promotheus.sh
 chmod +x remove.sh
-apt install sudo
-bash deb_docker.sh
+bash docker.sh
 bash glpi.sh
 bash promotheus.sh
-bash edit.sh nextcloud.sh nc.sh
-bash nc.sh
+bash nextcloud.sh
