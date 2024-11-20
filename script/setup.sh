1c0aae7e7c489e969a7bf2d6cfa469744500d2ec
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-20 20:44:56 +0100

    Update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+wget https://mgrosmann.vercel.app/script/agent-glpi.sh
 wget https://mgrosmann.vercel.app/script/docker.sh
 wget https://mgrosmann.vercel.app/script/glpi.sh
 wget https://mgrosmann.vercel.app/script/nextcloud.sh
 wget https://mgrosmann.vercel.app/script/promotheus.sh
 wget https://mgrosmann.vercel.app/script/remove.sh
+chmod +x agent-glpi.sh
 chmod +x docker.sh
 chmod +x glpi.sh
 chmod +x nextcloud.sh
 chmod +x promotheus.sh
 chmod +x remove.sh
 bash docker.sh
+bash agent-glpi.sh
 bash glpi.sh
 bash promotheus.sh
 bash nextcloud.sh
