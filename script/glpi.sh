39bd4f09358337f683e72ccc632625bd9275df1b
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-20 18:48:53 +0100

    Update glpi.sh

--- script/glpi.sh
+++ script/glpi.sh
@@ -46,7 +46,7 @@ wget https://github.com/glpi-project/glpi-agent/releases/download/1.7.3/glpi-age
 chmod +x glpi-agent-1.7.3-linux-installer.pl
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 echo -e "\e[31mVotre adresse IP est $IP_ADDRESS\e[0m"
-sudo ./glpi-agent-1.11-linux-installer.pl
+sudo ./glpi-agent-1.7.3-linux-installer.pl
 sudo bash -c "echo 'server = http://$IP_ADDRESS' > /etc/glpi-agent/glpi-agent.conf"
 sudo systemctl start glpi-agent
 sudo systemctl enable glpi-agent
