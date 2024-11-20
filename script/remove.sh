590f342411fd9ab40f581fb808203e10b5d6dcf4
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-20 13:40:37 +0100

    Update remove.sh

--- script/remove.sh
+++ script/remove.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+wget https://mgrosmann.vercel.app/script/fvcknextcloud.sh
+wget https://mgrosmann.vercel.app/script/nikphp.sh
+wget https://mgrosmann.vercel.app/script/uninstall.sh
 chmod +x fvcknextcloud.sh
 chmod +x nikphp.sh
 chmod +x uninstall.sh
