8d4a7ebbfbd07f1d50b512012b084c70ef77aacf
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-21 21:17:17 +0100

    Add files via upload

---
+++ script/dehek.sh
@@ -0,0 +1,15 @@
+sudo apt install sl
+sudo apt install cowsay
+#cowsay -f tux
+sudo apt install fortune
+#commande yes
+sudo apt install figlet
+#figlet "Linux"
+sudo apt install toilet
+#toilet --gay "Ubuntu"
+sudo apt install cmatrix
+#cmatrix
+sudo apt install lolcat
+#echo "Linux est génial !" | lolcat
+sudo apt install neofetch
+#neofetch
