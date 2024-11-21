c56dd06d86e6e39cd4ad67f47ec3444b8b84444c
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-21 14:40:14 +0100

    Update glpi.sh

--- script/glpi.sh
+++ script/glpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y apache2 mariadb-server php php-mysql php-xml php-mbstring php-curl php-gd php-intl php-ldap php-apcu
-sudo mysql_secure_installation <<EOF
+apt-get update && sudo apt-get upgrade -y
+ apt-get install -y apache2 mariadb-server php php-mysql php-xml php-mbstring php-curl php-gd php-intl php-ldap php-apcu
+mysql_secure_installation <<EOF
 n
 n
 n
@@ -9,7 +9,7 @@ n
 y
 y
 EOF
-sudo mysql <<EOF
+mysql <<EOF
 CREATE DATABASE glpi;
 CREATE USER 'glpi'@'localhost' IDENTIFIED BY 'glpi';
 GRANT ALL PRIVILEGES ON glpi.* TO 'glpi'@'localhost';
@@ -20,14 +20,14 @@ IP_ADDRESS=$(hostname -I | awk '{print $1}')
 echo -e "\e[31mVotre adresse IP est $IP_ADDRESS\e[0m"
 wget https://github.com/glpi-project/glpi/releases/download/10.0.17/glpi-10.0.17.tgz
 tar -xvzf glpi-10.0.17.tgz
-sudo mv glpi /var/www/html/
-sudo chown -R www-data:www-data /var/www/html/glpi
-sudo chmod -R 755 /var/www/html/glpi
-sudo bash -c 'cat <<EOT > /etc/apache2/sites-available/glpi.conf
+mv glpi /var/www/html/
+chown -R www-data:www-data /var/www/html/glpi
+chmod -R 755 /var/www/html/glpi
+bash -c 'cat <<EOT > /etc/apache2/sites-available/glpi.conf
 <VirtualHost *:80>
     ServerAdmin admin@example.com
     DocumentRoot /var/www/html/glpi
-    ServerName $IP_ADDRESS
+    ServerName 192.168.1.78
     <Directory /var/www/html/glpi>
         Options FollowSymLinks
         AllowOverride All
