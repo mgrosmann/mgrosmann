d401e6a3453e6c472316021852b37ada1c8c4e19
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-21 14:02:15 +0100

    Update agent-glpi.sh

--- script/agent-glpi.sh
+++ script/agent-glpi.sh
@@ -2,6 +2,6 @@
 wget https://github.com/glpi-project/glpi-agent/releases/download/1.7.3/glpi-agent-1.7.3-linux-installer.pl
 chmod +x /glpi-agent-1.7.3-linux-installer.pl
 sudo .//glpi-agent-1.7.3-linux-installer.pl
-sudo bash -c 'echo "server = http://http://172.25.217.120//glpi" > /etc/glpi-agent/glpi-agent.conf'
+sudo bash -c 'echo "server = http://http://192.168.1.30/glpi" > /etc/glpi-agent/glpi-agent.conf'
 sudo systemctl start glpi-agent
 sudo systemctl enable glpi-agent
