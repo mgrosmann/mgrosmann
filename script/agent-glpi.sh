3fcb6b7605ee4408eb720267a14e346f241b6f00
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-21 15:13:58 +0100

    Update agent-glpi.sh

--- script/agent-glpi.sh
+++ script/agent-glpi.sh
@@ -1,4 +1,4 @@
 wget https://github.com/glpi-project/glpi-agent/releases/download/1.11/glpi-agent-1.11-with-snap-linux-installer.pl
 chmod +x glpi-agent-1.11-with-snap-linux-installer.pl
-sudo ./glpi-agent-1.11-with-snap-linux-installer.pl
-sudo systemctl enable glpi-agent
+./glpi-agent-1.11-with-snap-linux-installer.pl
+systemctl enable glpi-agent
