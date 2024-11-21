312ca5e2ab9b61f10444aa6272087f86c58391a4
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-21 08:26:07 +0100

    Delete script/agent-glpi.sh

--- script/agent-glpi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-wget https://github.com/glpi-project/glpi-agent/releases/download/1.7.3/glpi-agent-1.7.3-linux-installer.pl
-chmod +x glpi-agent-1.7.3-linux-installer.pl
-IP_ADDRESS=$(hostname -I | awk '{print $1}')
-echo -e "\e[31mVotre adresse IP est $IP_ADDRESS\e[0m"
-sudo ./glpi-agent-1.7.3-linux-installer.pl
-sudo bash -c "echo 'server = http://$IP_ADDRESS/glpi' > /etc/glpi-agent/glpi-agent.conf"
-sudo systemctl start glpi-agent
-sudo systemctl enable glpi-agent
