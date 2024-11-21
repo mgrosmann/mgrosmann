750fa4298922e146a101893e8188c3a228b358a4
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-21 15:29:37 +0100

    Update agent-glpi.sh

--- script/agent-glpi.sh
+++ script/agent-glpi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt install perl
 wget https://github.com/glpi-project/glpi-agent/releases/download/1.7.1/glpi-agent-1.7.1-linux-installer.pl
-perl glpi-agent-1.7.1-linux-installer.pl -s http://192.168.1.207/front/inventory.php --runnow --install
+perl glpi-agent-1.7.1-linux-installer.pl -s http://192.168.1.78/glpi --runnow --install
 systemctl enable glpi-agent
