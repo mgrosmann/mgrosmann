80a56499c8f00ae847efe1754fb9cdc645ede41e
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-23 16:46:48 +0100

    update mysql_dump.sh

--- script/mysql_dump.sh
+++ script/mysql_dump.sh
@@ -4,7 +4,5 @@ DB_PASS="root"
 DATE=$(date +"%Y%m%d%H%M")
 SOURCE_DIR="/home/mgrosmann/desktop"
 mkdir -p $SOURCE_DIR
-# Sauvegarder toutes les bases de données, y compris les utilisateurs et les privilèges
 mysqldump -u $DB_USER -p$DB_PASS --all-databases > $SOURCE_DIR/all-databases-$DATE.sql
-# Message de confirmation
 echo "La sauvegarde de la base de données MySQL a été effectuée avec succès et copiée vers $SOURCE_DIR."
