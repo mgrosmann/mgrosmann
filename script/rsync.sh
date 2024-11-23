6ca98b11918b5685ac6bf7ef3a2107900163f670
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-23 12:00:07 +0100

    Update rsync.sh

--- script/rsync.sh
+++ script/rsync.sh
@@ -8,26 +8,3 @@ sudo apt install -y rsync
 mkdir -p /home/mgrosmann/backup
 mkdir -p /home/mgrosmann/desktop
 mkdir -p /home/mgrosmann/save
-
-# Variables de configuration
-SOURCE_DIR="/home/mgrosmann/desktop"
-BACKUP_DIR="/home/mgrosmann/backup"
-REMOTE_USER="mgrosmann"
-REMOTE_HOST="192.168.1.45"
-REMOTE_DIR="/home/mgrosmann/save"
-
-# Créer un fichier de test
-echo "test de la configuration de rsync" > $SOURCE_DIR/test.txt
-
-
-# Sauvegarder le répertoire local vers le serveur distant
-rsync -avz $SOURCE_DIR $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR
-
-# Supprimer le fichier de test local
-rm $SOURCE_DIR/test.txt
-
-# Restaurer le répertoire depuis le serveur distant vers le répertoire de restauration local
-rsync -avz $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR $BACKUP_DIR
-
-# Message de confirmation
-echo "La sauvegarde et la restauration avec rsync ont été effectuées avec succès."
