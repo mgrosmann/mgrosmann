2195c910d8cf5f3bf1c64c9330692a97c83bb5a9
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-23 12:26:42 +0100

    Update debian.sh

--- script/debian.sh
+++ script/debian.sh
@@ -3,21 +3,13 @@ wget https://mgrosmann.vercel.app/script/agent-glpi.sh
 wget https://mgrosmann.vercel.app/script/deb_docker.sh
 wget https://mgrosmann.vercel.app/script/glpi.sh
 wget https://mgrosmann.vercel.app/script/nextcloud.sh
-wget https://mgrosmann.vercel.app/script/mysql.sh
-wget https://mgrosmann.vercel.app/script/mysql_dump.sh
 wget https://mgrosmann.vercel.app/script/rsync.sh
-wget https://mgrosmann.vercel.app/script/save.sh
-wget https://mgrosmann.vercel.app/script/backup.sh
 wget https://mgrosmann.vercel.app/script/promotheus.sh
 wget https://mgrosmann.vercel.app/script/remove.sh
 chmod +x agent-glpi.sh
 chmod +x deb_docker.sh
 chmod +x glpi.sh
-chmod +x mysql.sh
-chmod +x mysql_dump.sh
 chmod +x rsync.sh
-chmod +x save.sh
-chmod +x backup.sh
 chmod +x nextcloud.sh
 chmod +x promotheus.sh
 chmod +x remove.sh
