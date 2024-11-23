9c4461c580389828e679af9e9ef659f43b73bdec
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-23 12:49:16 +0100

    Update dmz.sh

--- script/dmz.sh
+++ script/dmz.sh
@@ -6,6 +6,7 @@ iptables -A INPUT -p tcp --dport 22 -s $ALLOWED_IP -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j DROP
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
 iptables -A INPUT -j DROP
 iptables-save > /etc/iptables/rules.v4
 sed -i '/^#PermitRootLogin/s/^#//' /etc/ssh/sshd_config
@@ -14,4 +15,4 @@ sed -i '/^#PasswordAuthentication/s/^#//' /etc/ssh/sshd_config
 sed -i '/^PasswordAuthentication/s/yes/no/' /etc/ssh/sshd_config
 echo "AllowUsers utilisateur@$ALLOWED_IP" >> /etc/ssh/sshd_config
 systemctl restart sshd
-echo "La configuration de la machine en DMZ a été effectuée avec succès."
+echo "La configuration de la machine en DMZ  a été effectuée avec succès."
