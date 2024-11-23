f41a64ea3e1c73e4c583fe52f6a0475b177ccb3e
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-23 12:19:02 +0100

    Update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -3,10 +3,12 @@ wget https://mgrosmann.vercel.app/script/agent-glpi.sh
 wget https://mgrosmann.vercel.app/script/docker.sh
 wget https://mgrosmann.vercel.app/script/glpi.sh
 wget https://mgrosmann.vercel.app/script/mysql.sh
-wget https://mgrosmann.vercel.app/script/save_db.sh
+wget https://mgrosmann.vercel.app/script/mysql_dump.sh
 wget https://mgrosmann.vercel.app/script/nextcloud.sh
 wget https://mgrosmann.vercel.app/script/promotheus.sh
 wget https://mgrosmann.vercel.app/script/rsync.sh
+wget https://mgrosmann.vercel.app/script/save.sh
+wget https://mgrosmann.vercel.app/script/backup.sh
 wget https://mgrosmann.vercel.app/script/remove.sh
 chmod +x agent-glpi.sh
 chmod +x docker.sh
@@ -16,6 +18,9 @@ chmod +x save_db.sh
 chmod +x nextcloud.sh
 chmod +x promotheus.sh
 chmod +x rsync.sh
+chmod +x mysql_dump.sh
+chmod +x save.sh
+chmod +x backup.sh
 chmod +x remove.sh
 bash docker.sh
 bash promotheus.sh
