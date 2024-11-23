38ecfe098d5a7866d277283b1fe0e55bd7e5783a
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-23 18:10:21 +0100

    update dhcp.sh

--- script/dhcp.sh
+++ script/dhcp.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
+# Nom de l'interface réseau (remplacez par le nom de votre interface, par exemple enp0s3)
 INTERFACE="enp0s3"
+
+# Mettre à jour les paquets et installer dhclient si nécessaire
 sudo apt update
-sudo apt install isc-dhcp-client
+sudo apt install -y isc-dhcp-client
+
 # Libérer l'ancienne adresse IP
 sudo dhclient -r $INTERFACE
+
+# Désactiver l'interface réseau
+sudo ip link set $INTERFACE down
+
+# Réactiver l'interface réseau
+sudo ip link set $INTERFACE up
 
 # Redémarrer le service réseau pour obtenir une nouvelle adresse IP
 sudo systemctl restart systemd-networkd
