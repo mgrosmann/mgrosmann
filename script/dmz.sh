1b7759ab016763397353a58d8e62623ca46fbc65
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-23 12:45:46 +0100

    Create dmz.sh

---
+++ script/dmz.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ALLOWED_IP="192.168.1.80"
+iptables -F
+iptables -X
+iptables -A INPUT -p tcp --dport 22 -s $ALLOWED_IP -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j DROP
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -j DROP
+iptables-save > /etc/iptables/rules.v4
+sed -i '/^#PermitRootLogin/s/^#//' /etc/ssh/sshd_config
+sed -i '/^PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
+sed -i '/^#PasswordAuthentication/s/^#//' /etc/ssh/sshd_config
+sed -i '/^PasswordAuthentication/s/yes/no/' /etc/ssh/sshd_config
+echo "AllowUsers utilisateur@$ALLOWED_IP" >> /etc/ssh/sshd_config
+systemctl restart sshd
+echo "La configuration de la machine en DMZ a été effectuée avec succès."
