9e65bffbcb7f95765d69f18b14b515f5f166a7df
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-23 12:30:28 +0100

    Update rsync.sh

--- script/rsync.sh
+++ script/rsync.sh
@@ -8,3 +8,13 @@ sudo apt install -y rsync
 mkdir -p /home/mgrosmann/backup
 mkdir -p /home/mgrosmann/desktop
 mkdir -p /home/mgrosmann/save
+wget https://mgrosmann.vercel.app/script/mysql.sh
+wget https://mgrosmann.vercel.app/script/mysql_dump.sh
+wget https://mgrosmann.vercel.app/script/save.sh
+wget https://mgrosmann.vercel.app/script/backup.sh
+chmod +x mysql.sh
+chmod +x mysql_dump.sh
+chmod +x save.sh
+chmod +x backup.sh
+bash mysql.sh
+bash mysql_dump.sh
