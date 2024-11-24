c545df9ff5e2769b2c3b447fa4b049231162fb61
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-24 12:02:08 +0100

    update ssh.sh

--- script/ssh.sh
+++ script/ssh.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-ssh-keygen -t rsa -b 2048 
-ssh-copy-id -i ~/.ssh/id_rsa.pub user@192.168.1.AAA
-ssh-copy-id -i ~/.ssh/id_rsa.pub user@192.168.1.BBB
-
-
-
-
-
-#1. **Sur la machine principale (Machine A) :
-#1. Générer une paire de clés SSH (sans passphrase) : ssh-keygen -t rsa -b 2048 
-#puis verifier avec ls ~/.ssh
-#ssh-copy-id -i ~/.ssh/id_rsa.pub user@192.168.1.AAA
-#ssh-copy-id -i ~/.ssh/id_rsa.pub user@192.168.1.BBB
-#2. puis sur les deux autres machines
-#user@192.168.1.AAA
-#ssh-keygen -t rsa -b 2048
-#ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu@192.168.1.100
-#user@192.168.1.BBB
-#ssh-keygen -t rsa -b 2048
-#ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu@192.168.1.100
+ssh-keygen -t rsa -b 2048 -f /root/ssh.txt
+ssh-copy-id -i /root/ssh.txt.pub user@192.168.1.AAA
+ssh-copy-id -i /root/ssh.txt.pub user@192.168.1.BBB
