f5b33527394a51ce0bce2562f6741f1602d881c9
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-24 12:05:19 +0100

    upgrade ssh

--- script/ssh.sh
+++ script/ssh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ssh-keygen -t rsa -b 2048 -f /root/ssh.txt
+ssh-keygen -t rsa -b 2048 -f /root/ssh.txt -N ""
 ssh-copy-id -i /root/ssh.txt.pub user@192.168.1.AAA
 ssh-copy-id -i /root/ssh.txt.pub user@192.168.1.BBB
