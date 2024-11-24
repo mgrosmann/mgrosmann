5b583a89a617c4bbbc3e42c1bdfa9c1cde063a0f
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-24 12:32:00 +0100

    update ssh.sh

--- script/ssh.sh
+++ script/ssh.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# Générer la clé SSH sans mot de passe et la sauvegarder dans /root/ssh.txt
 ssh-keygen -t rsa -b 2048 -f /root/ssh.txt -N ""
-
-# Copier la clé publique vers les machines distantes
 ssh-copy-id -i /root/ssh.txt.pub mgrosmann@192.168.1.110
 ssh-copy-id -i /root/ssh.txt.pub mgrosmann@192.168.1.111
