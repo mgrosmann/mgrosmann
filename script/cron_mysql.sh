6d7ba16c082147364526781070b35b8a96196c02
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-27 11:20:15 +0100

    Update cron_mysql.sh

--- script/cron_mysql.sh
+++ script/cron_mysql.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 DB_USER="root"
 DB_PASS="root"
-DATE=$(date +"%Y%m%d%H%M")
 SOURCE_DIR="/home/mgrosmann/desktop"
-mkdir -p $SOURCE_DIR
-mysqldump -u $DB_USER -p$DB_PASS --all-databases > $SOURCE_DIR/all-databases-$DATE.sql
-echo "* * * * * root mysqldump -u $DB_USER -p$DB_PASS --all-databases > /root/save.mysql" >> /etc/crontab
+mkdir -p "$SOURCE_DIR"
+echo "* * * * * root mysqldump -u $DB_USER -p$DB_PASS --all-databases > /root/all-databases-\$(date +\%Y\%m\%d\%H\%M).sql" >> /etc/crontab
