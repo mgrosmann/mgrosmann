19906d002b3798d95fc60148f8b58fb37383b4f4
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-27 16:59:27 +0100

    Update bin.sh

--- script/bin.sh
+++ script/bin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget mgrosmann.vercel.app/script/https.sh
-wget mgrosmann.vercel.app/script/https.sh
+wget mgrosmann.vercel.app/script/site.sh
 apt install dos2unix
 dos2unix *.sh
 cat https.sh >/bin/https
