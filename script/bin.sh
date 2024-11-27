c6867a85f56a87784cd1edc82d01d85bed07a509
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-27 16:02:53 +0100

    Create bin.sh

---
+++ script/bin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget mgrosmann.vercel.app/script/https.sh
+wget mgrosmann.vercel.app/script/https.sh
+apt install dos2unix
+dos2unix *.sh
+cat https.sh >/bin/https
+cat site.sh >/bin/site
+cd /bin
+chmod +x *
