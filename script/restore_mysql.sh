647aa1a6b6108aa7adc24ad119b09a2faf5af48e
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-11-27 10:49:43 +0100

    Update restore_mysql.sh

--- script/restore_mysql.sh
+++ script/restore_mysql.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 DB_USER="root"
 DB_PASS="root"
-SOURCE_DIR="/home/mgrosmann/desktop"
-LATEST_BACKUP=$(ls -t $SOURCE_DIR/all-databases-*.sql | head -n 1)
-mysql -u $DB_USER -p$DB_PASS < "$LATEST_BACKUP"
+SOURCE_DIR="/home/mgrosmann/desktop
+LATEST_BACKUP=$(ls -t "$SOURCE_DIR"/all-databases-*.sql 2>/dev/null | head -n 1)
+if [ -z "$LATEST_BACKUP" ]; then
+  echo "Aucune sauvegarde trouvée dans $SOURCE_DIR."
+  exit 1
+fi
+mysql -u "$DB_USER" -p"$DB_PASS" < "$LATEST_BACKUP"
 echo "La sauvegarde la plus récente ($LATEST_BACKUP) a été restaurée avec succès."
