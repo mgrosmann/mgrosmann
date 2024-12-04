df444a4bc4fb44fcb628866752895ec2a6c400dc
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-04 21:01:48 +0100

    update container

--- script/docker/container.sh
+++ script/docker/container.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-echo "Que voulez-vous faire ?"
-echo "1) Créer un  nouveau conteneur avec l'image HTTPD ou en modifier un existant"
-echo "2) installer le conteneur multi-service (MySQL, phpMyAdmin, FTP, APACHE)"
-echo "3) Se connecter à un conteneur"
-echo "4) Démarrer tous les conteneurs"
-echo "5) Arrêter tous les conteneurs"
-echo "6) Installer nano et wget sur un conteneur"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+PINK='\033[1;35m'
+PURPLE='\033[0;35m'
+ORANGE='\033[0;33m'
+BROWN='\033[0;33m'
+LIGHT_BLUE='\033[1;34m'
+LIGHT_GREEN='\033[1;32m'
+NC='\033[0m'
+echo -e "${RED}1)${NC} Créer un nouveau ${GREEN}conteneur${NC} avec l'image ${BLUE}HTTPD${NC} ou en modifier un ${GREEN}existant${NC}"
+echo -e "${RED}2)${NC} Installer le ${GREEN}conteneur${NC} multi-service (${PINK}MySQL${NC}, ${RED}phpMyAdmin${NC}, ${ORANGE}FTP${NC}, ${LIGHT_BLUE}APACHE${NC})"
+echo -e "${RED}3)${NC} Se connecter à un ${GREEN}conteneur${NC}"
+echo -e "${RED}4)${NC} Démarrer tous les ${GREEN}conteneurs${NC}"
+echo -e "${RED}5)${NC} Arrêter tous les ${GREEN}conteneurs${NC}"
+echo -e "${RED}6)${NC} Installer ${BLUE}nano${NC} et ${YELLOW}wget${NC} sur un ${GREEN}conteneur${NC}"
 read -p "Entrez le numéro de votre choix: " choix
 case $choix in
     1)
