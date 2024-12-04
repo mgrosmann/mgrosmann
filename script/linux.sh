4ef23710e4f25a865790ee8162d10cc124bc95be
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-04 15:44:13 +0100

    update linux.sh

--- script/linux.sh
+++ script/linux.sh
@@ -3,3 +3,7 @@ read -p "sur quel conteneur voulez vous appliquez des commandes linux" name
   docker cp "/bin/wget" "$name:/bin"
   docker cp "/bin/touch" "$name:/bin"
   docker cp "/bin/nano" "$name:/bin"
+chmod +x wget
+chmod +x touch
+chmod +x nano
+echo "Commandes linux copiées avec succès dans le conteneur $name"
