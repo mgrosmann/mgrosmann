0ac390492bd82df3ef30c68cd810e8945b9fd934
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-04 14:45:05 +0100

    update docker

--- script/start.sh
+++ script/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 for file in *.yaml
 do 
-    docker compose -f "$file" start
-  else
-    echo "Aucun fichier .yaml trouvé."
-  fi
+  docker compose -f $file stop ; 
 done
--- script/stop.sh
+++ script/stop.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 for file in *.yaml
 do 
-    docker compose -f "$file" stop
-  else
-    echo "Aucun fichier .yaml trouvé."
-  fi
+  docker compose -f $file stop ; 
 done
