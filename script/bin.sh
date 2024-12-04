4a269c87afe9172b453ba0fc1bf4c22537727231
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-04 20:32:50 +0100

    container

--- script/bin.sh
+++ script/bin.sh
@@ -22,6 +22,7 @@ wget mgrosmann.vercel.app/script/docker/ssh_ct.sh
 wget mgrosmann.vercel.app/script/docker/start.sh
 wget mgrosmann.vercel.app/script/docker/stop.sh
 wget mgrosmann.vercel.app/script/docker/linux.sh
+wget mgrosmann.vercel.app/script/docker/container.sh
 dos2unix *.sh
 cat https.sh > /usr/local/bin/https
 cat site.sh > /usr/local/bin/site
@@ -31,5 +32,7 @@ cat ssh_ct.sh > /usr/local/bin/ct-connect
 cat start.sh > /usr/local/bin/ct-start
 cat stop.sh > /usr/local/bin/ct-stop
 cat linux.sh > /usr/local/bin/ct-linux
+cat container.sh > /usr/local/bin/container
+
 cd /usr/local/bin
 chmod +x *
