b03f221d74be5c32afa94aa74a8ed911f5e5c338
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-04 14:33:43 +0100

    update stop.sh

--- script/stop.sh
+++ script/stop.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 for file in *.yaml; do
   if [ -f "$file" ]; then
-    echo "Arrêt des services définis dans $file"
     docker compose -f "$file" stop
   else
     echo "Aucun fichier .yaml trouvé."
