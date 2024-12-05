0a12d52dde3d4e8473a6bdfa959f345ab0f1cb28
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-05 17:29:03 +0100

    update pma.sh

--- script/docker/pma.sh
+++ script/docker/pma.sh
@@ -1,37 +1,25 @@
 #!/bin/bash
-read -p "port myql" port_sql
-read -p "port pma" port_pma
-read -p "nom projet pma" name
-cat <<EOF > $name.yaml
+read -p "Port MySQL: " port_sql
+read -p "Port phpMyAdmin: " port_pma
+read -p "Nom du projet: " name
+cat <<EOF > docker-compose.yml
 services:
-
-  db_$name:
-    container_name: db_$name
+  db:
     image: mysql
-    ports:
-      - "$port_sql:3306"
+    container_name: db_$name
     environment:
       MYSQL_ROOT_PASSWORD: root
-      MYSQL_DATABASE: mysql_db_$name
-      MYSQL_USER: mgrosmann
-      MYSQL_PASSWORD: password
-    networks:
-      - "network_$name"
+    ports:
+      - "$port_sql:3306"
 
-  phpmyadmin_$name:
+  phpmyadmin:
+    image: phpmyadmin/phpmyadmin
     container_name: phpmyadmin_$name
-    image: phpmyadmin
+    environment:
+      PMA_HOST: db
+      MYSQL_ROOT_PASSWORD: root
     ports:
       - "$port_pma:80"
-    environment:
-      HOST: mysql_db_$name
-      USERNAME: mgrosmann
-      PASSWORD: password
-    depends_on:
-      - "db_$name"
-    networks:
-      - "network_$name"
-networks:
-  network_$name:
 EOF
-docker compose -f "$name.yaml" up -d
+
+docker-compose up -d
