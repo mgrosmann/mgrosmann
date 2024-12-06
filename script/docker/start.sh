c18ce0539f4c8f33465419962c201c7d34a285cc
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-06 11:46:15 +0100

    d

--- script/docker/start.sh
+++ script/docker/start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-for file in *.yaml
-do
-  docker compose -f $file start ;
+for file in *.yaml; do
+  docker compose -f "$file" start
 done
-docker ps -a -q | xargs -r docker start
+docker ps -a -q | while read container_id; do
+  networks=$(docker inspect --format '{{range $k, $v := .NetworkSettings.Networks}}{{$k}} {{end}}' "$container_id")
+  for network in $networks; do
+    if ! docker network ls | grep -qw "$network"; then
+      docker network create "$network"
+    fi
+  done
+  docker start "$container_id"
+done
