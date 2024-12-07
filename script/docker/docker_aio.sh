8300ea541d138c176198f05685f739a0316c5272
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-07 08:11:09 +0100

    update container

--- script/docker/docker_aio.sh
+++ script/docker/docker_aio.sh
@@ -14,89 +14,141 @@ LIGHT_GREEN='\033[1;32m'
 BROWN='\033[0;33m'
 GRAY='\033[1;30m'
 NC='\033[0m'
+echo -e "${LIGHT_GREEN}Veuillez choisir une catégorie :${NC}"
+echo -e "${RED}1)${NC} Conteneur interactif"
+echo -e "${RED}2)${NC} Conteneur détaché"
 
-echo -e "${LIGHT_GREEN}1)${NC} Créer un conteneur interactif Apache HTTPD"
-echo -e "${LIGHT_GREEN}2)${NC} Créer un conteneur interactif Apache HTTPD avec volume monté"
-echo -e "${LIGHT_GREEN}3)${NC} Créer un conteneur interactif MySQL sans mot de passe"
-echo -e "${LIGHT_GREEN}4)${NC} Créer un conteneur interactif MySQL avec mot de passe"
-echo -e "${LIGHT_GREEN}5)${NC} Créer un conteneur intéractif PhpMyadmin"
-echo -e "${LIGHT_GREEN}6)${NC} Créer un conteneur détaché Apache HTTPD"
-echo -e "${LIGHT_GREEN}7)${NC} Créer un conteneur détaché Apache HTTPD avec volume monté"
-echo -e "${LIGHT_GREEN}8)${NC} Créer un conteneur détaché MySQL sans mot de passe"
-echo -e "${LIGHT_GREEN}9)${NC} Créer un conteneur détaché MySQL avec mot de passe"
-echo -e "${LIGHT_GREEN}10)${NC} Créer un conteneur détaché PhpMyadmin"
-echo -e "${LIGHT_GREEN}11)${NC} Créer un conteneur interactif Ubuntu SSH avec utilisateur et mot de passe"
-echo -e "${LIGHT_GREEN}12)${NC} Créer un conteneur détaché Ubuntu SSH avec utilisateur et mot de passe"
+read -p "Entrez le numéro de votre choix : " categorie
 
-read -p "Entrez le numéro de votre choix: " choix
+if [ "$categorie" -eq 1 ]; then
+    echo -e "\n${LIGHT_GREEN}Conteneur interactif :${NC}"
+    echo -e "${LIGHT_GREEN}1)${NC} Apache HTTPD"
+    echo -e "${LIGHT_GREEN}2)${NC} MySQL"
+    echo -e "${LIGHT_GREEN}3)${NC} PhpMyAdmin"
+    echo -e "${LIGHT_GREEN}4)${NC} Ubuntu SSH avec utilisateur et mot de passe"
 
-if [ "$choix" -eq 1 ]; then
-    read -p "Entrez le port à utiliser : " port
-    read -p "Entrez le nom du conteneur : " name
-    docker container run -it -p $port:80 --name $name httpd
-elif [ "$choix" -eq 2 ]; then
-    read -p "Entrez le port à utiliser : " port
-    read -p "Entrez le nom du conteneur : " name
-    read -p "Entrez le nom du repertoire : " repertory
-    docker container run -it -p $port:80 -v $repertory:/usr/local/apache2/htdocs --name $name httpd
-elif [ "$choix" -eq 3 ]; then
-    read -p "Entrez le port à utiliser  : " port
-    read -p "Entrez le nom du conteneur : " name
-    docker container run -it -p $port:3306 --name $name mysql
-elif [ "$choix" -eq 4 ]; then
-    read -p "Entrez le port à utiliser  : " port
-    read -p "Entrez le nom du conteneur : " name
-    read -p "Entrez le mot de passe root MySQL: " root_password
-    docker container run -it -p $port:3306 --name $name -e MYSQL_ROOT_PASSWORD=$root_password mysql
-elif [ "$choix" -eq 5 ]; then
-    read -p "Entrez le por sql à utiliser : " port_sql
-    read -p "Entrez le port pma à utiliser : " port_pma
-    read -p "Entrez le mot de passe du root à utiliser : " mdp_root
-    read -p "Entrez le nom du conteneur : " name
-    docker network create network-$name
-    docker run -d --name sql_$name --network network-$name -e MYSQL_ROOT_PASSWORD=$mdp_root -p $port_sql:3306 mysql
-    docker run -it --name pma_$name --network network-$name -e PMA_HOST=sql_$name -e MYSQL_ROOT_PASSWORD=$mdp_root -p $port_pma:80 phpmyadmin/phpmyadmin
-elif [ "$choix" -eq 6 ]; then
-    read -p "Entrez le port à utiliser : " port
-    read -p "Entrez le nom du conteneur : " name
-    docker container run -d -p $port:80 --name $name httpd
-elif [ "$choix" -eq 7 ]; then
-    read -p "Entrez le port à utiliser : " port
-    read -p "Entrez le nom du conteneur : " name
-    read -p "Entrez le nom du repertoire : " repertory
-    docker container run -d -p $port:80 -v $repertory:/usr/local/apache2/htdocs --name $name httpd
-elif [ "$choix" -eq 8 ]; then
-    read -p "Entrez le port à utiliser : " port
-    read -p "Entrez le nom du conteneur : " name
-    docker container run -d -p $port:3306 --name $name mysql
-elif [ "$choix" -eq 9 ]; then
-    read -p "Entrez le port à utiliser : " port
-    read -p "Entrez le nom du conteneur : " name
-    read -p "Entrez le mot de passe root MySQL: " root_password
-    docker container run -d -p $port:3306 --name $name -e MYSQL_ROOT_PASSWORD=$root_password mysql
+    read -p "Entrez le numéro de votre choix : " choix_interactif
 
-elif [ "$choix" -eq 10 ]; then
-    read -p "Entrez le por sql à utiliser : " port_sql
-    read -p "Entrez le port pma à utiliser : " port_pma
-    read -p "Entrez le mot de passe du root à utiliser : " mdp_root
-    read -p "Entrez le nom du conteneur : " name
-    docker network create network-$name
-    docker run -d --name sql_$name --network network-$name -e MYSQL_ROOT_PASSWORD=$mdp_root -p $port_sql:3306 mysql
-    docker run -d --name pma_$name --network network-$name -e PMA_HOST=sql_$name -e MYSQL_ROOT_PASSWORD=$mdp_root -p $port_pma:80 phpmyadmin/phpmyadmin
-elif [ "$choix" -eq 11 ]; then
-    read -p "Entrez le port SSH à utiliser : " port
-    read -p "Entrez le nom du conteneur : " name
-    read -p "Entrez le nom de l'utilisateur : " user
-    read -p "Entrez le mot de passe : " password
-    docker container run -it -p $port:22 --name $name rastasheep/ubuntu-sshd && \
-    docker exec -it $name /bin/bash -c "adduser --disabled-password --gecos '' $user && echo '$user:$password' | chpasswd"
-elif [ "$choix" -eq 12 ]; then
-    read -p "Entrez le port SSH à utiliser : " port
-    read -p "Entrez le nom du conteneur : " name
-    read -p "Entrez le nom de l'utilisateur : " user
-    read -p "Entrez le mot de passe : " password
-    docker container run -d -p $port:22 --name $name rastasheep/ubuntu-sshd && \
-    docker exec -it $name /bin/bash -c "adduser --disabled-password --gecos '' $user && echo '$user:$password' | chpasswd"
+    if [ "$choix_interactif" -eq 1 ]; then
+        echo -e "${LIGHT_GREEN}Apache HTTPD :${NC}"
+        echo -e "  ${LIGHT_GREEN}1)${NC} Sans volume monté"
+        echo -e "  ${LIGHT_GREEN}2)${NC} Avec volume monté"
+        read -p "Entrez le numéro de votre choix : " choix_httpd
+        if [ "$choix_httpd" -eq 1 ]; then
+            read -p "Entrez le port à utiliser : " port
+            read -p "Entrez le nom du conteneur : " name
+            docker container run -it -p $port:80 --name $name httpd
+        elif [ "$choix_httpd" -eq 2 ]; then
+            read -p "Entrez le port à utiliser : " port
+            read -p "Entrez le nom du conteneur : " name
+            read -p "Entrez le nom du répertoire : " repertory
+            docker container run -it -p $port:80 -v $repertory:/usr/local/apache2/htdocs --name $name httpd
+        else
+            echo -e "${RED}Choix invalide pour HTTPD.${NC}"
+        fi
+
+    elif [ "$choix_interactif" -eq 2 ]; then
+        echo -e "${LIGHT_GREEN}MySQL :${NC}"
+        echo -e "  ${LIGHT_GREEN}1)${NC} Sans mot de passe"
+        echo -e "  ${LIGHT_GREEN}2)${NC} Avec mot de passe"
+        read -p "Entrez le numéro de votre choix : " choix_mysql
+        if [ "$choix_mysql" -eq 1 ]; then
+            read -p "Entrez le port à utiliser : " port
+            read -p "Entrez le nom du conteneur : " name
+            docker container run -it -p $port:3306 --name $name mysql
+        elif [ "$choix_mysql" -eq 2 ]; then
+            read -p "Entrez le port à utiliser : " port
+            read -p "Entrez le nom du conteneur : " name
+            read -p "Entrez le mot de passe root MySQL : " root_password
+            docker container run -it -p $port:3306 --name $name -e MYSQL_ROOT_PASSWORD=$root_password mysql
+        else
+            echo -e "${RED}Choix invalide pour MySQL.${NC}"
+        fi
+
+    elif [ "$choix_interactif" -eq 3 ]; then
+        read -p "Entrez le port SQL à utiliser : " port_sql
+        read -p "Entrez le port PhpMyAdmin à utiliser : " port_pma
+        read -p "Entrez le mot de passe root MySQL : " mdp_root
+        read -p "Entrez le nom du conteneur : " name
+        docker network create network-$name
+        docker run -d --name sql_$name --network network-$name -e MYSQL_ROOT_PASSWORD=$mdp_root -p $port_sql:3306 mysql
+        docker run -it --name pma_$name --network network-$name -e PMA_HOST=sql_$name -e MYSQL_ROOT_PASSWORD=$mdp_root -p $port_pma:80 phpmyadmin/phpmyadmin
+
+    elif [ "$choix_interactif" -eq 4 ]; then
+        read -p "Entrez le port SSH à utiliser : " port
+        read -p "Entrez le nom du conteneur : " name
+        read -p "Entrez le nom de l'utilisateur : " user
+        read -p "Entrez le mot de passe : " password
+        docker container run -it -p $port:22 --name $name rastasheep/ubuntu-sshd && \
+        docker exec -it $name /bin/bash -c "adduser --disabled-password --gecos '' $user && echo '$user:$password' | chpasswd"
+    else
+        echo -e "${RED}Choix invalide pour les conteneurs interactifs.${NC}"
+    fi
+
+elif [ "$categorie" -eq 2 ]; then
+    echo -e "\n${LIGHT_GREEN}Conteneur détaché :${NC}"
+    echo -e "${LIGHT_GREEN}1)${NC} Apache HTTPD"
+    echo -e "${LIGHT_GREEN}2)${NC} MySQL"
+    echo -e "${LIGHT_GREEN}3)${NC} PhpMyAdmin"
+    echo -e "${LIGHT_GREEN}4)${NC} Ubuntu SSH avec utilisateur et mot de passe"
+
+    read -p "Entrez le numéro de votre choix : " choix_detache
+
+    if [ "$choix_detache" -eq 1 ]; then
+        echo -e "${LIGHT_GREEN}Apache HTTPD :${NC}"
+        echo -e "  ${LIGHT_GREEN}1)${NC} Sans volume monté"
+        echo -e "  ${LIGHT_GREEN}2)${NC} Avec volume monté"
+        read -p "Entrez le numéro de votre choix : " choix_httpd
+        if [ "$choix_httpd" -eq 1 ]; then
+            read -p "Entrez le port à utiliser : " port
+            read -p "Entrez le nom du conteneur : " name
+            docker container run -d -p $port:80 --name $name httpd
+        elif [ "$choix_httpd" -eq 2 ]; then
+            read -p "Entrez le port à utiliser : " port
+            read -p "Entrez le nom du conteneur : " name
+            read -p "Entrez le nom du répertoire : " repertory
+            docker container run -d -p $port:80 -v $repertory:/usr/local/apache2/htdocs --name $name httpd
+        else
+            echo -e "${RED}Choix invalide pour HTTPD.${NC}"
+        fi
+
+    elif [ "$choix_detache" -eq 2 ]; then
+        echo -e "${LIGHT_GREEN}MySQL :${NC}"
+        echo -e "  ${LIGHT_GREEN}1)${NC} Sans mot de passe"
+        echo -e "  ${LIGHT_GREEN}2)${NC} Avec mot de passe"
+        read -p "Entrez le numéro de votre choix : " choix_mysql
+        if [ "$choix_mysql" -eq 1 ]; then
+            read -p "Entrez le port à utiliser : " port
+            read -p "Entrez le nom du conteneur : " name
+            docker container run -d -p $port:3306 --name $name mysql
+        elif [ "$choix_mysql" -eq 2 ]; then
+            read -p "Entrez le port à utiliser : " port
+            read -p "Entrez le nom du conteneur : " name
+            read -p "Entrez le mot de passe root MySQL : " root_password
+            docker container run -d -p $port:3306 --name $name -e MYSQL_ROOT_PASSWORD=$root_password mysql
+        else
+            echo -e "${RED}Choix invalide pour MySQL.${NC}"
+        fi
+
+    elif [ "$choix_detache" -eq 3 ]; then
+        read -p "Entrez le port SQL à utiliser : " port_sql
+        read -p "Entrez le port PhpMyAdmin à utiliser : " port_pma
+        read -p "Entrez le mot de passe root MySQL : " mdp_root
+        read -p "Entrez le nom du conteneur : " name
+        docker network create network-$name
+        docker run -d --name sql_$name --network network-$name -e MYSQL_ROOT_PASSWORD=$mdp_root -p $port_sql:3306 mysql
+        docker run -d --name pma_$name --network network-$name -e PMA_HOST=sql_$name -e MYSQL_ROOT_PASSWORD=$mdp_root -p $port_pma:80 phpmyadmin/phpmyadmin
+
+    elif [ "$choix_detache" -eq 4 ]; then
+        read -p "Entrez le port SSH à utiliser : " port
+        read -p "Entrez le nom du conteneur : " name
+        read -p "Entrez le nom de l'utilisateur : " user
+        read -p "Entrez le mot de passe : " password
+        docker container run -d -p $port:22 --name $name rastasheep/ubuntu-sshd && \
+        docker exec -it $name /bin/bash -c "adduser --disabled-password --gecos '' $user && echo '$user:$password' | chpasswd"
+    else
+        echo -e "${RED}Choix invalide pour les conteneurs détachés.${NC}"
+    fi
 else
-    echo -e "${RED}Choix invalide. Veuillez réessayer.${NC}"
+    echo -e "${RED}Catégorie invalide.${NC}"
 fi
