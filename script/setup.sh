9a21f4650c3c5da9c8e7d33acadc2c4c50998e4a
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-07 10:52:55 +0100

    Delete script/setup.sh

--- script/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if ! command -v git &> /dev/null; then
-    apt-get update && sudo apt-get install git -y
-else
-    echo "Git est déjà installé."
-fi
-apt install git -y && git clone https://github.com/mgrosmann/docker.git
-cd docker && chmod +x bin.sh && bash bin.sh
-echo "lancer la commande 'container'"
