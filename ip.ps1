3721a12e8a099c37c0eb221decf19d0b4beeebba
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-09 16:56:49 +0100

    Update ip.ps1

--- ip.ps1
+++ ip.ps1
@@ -1,16 +1,17 @@
-$InterfaceAlias = "Ethernet" 
-$IPAddress = "192.168.1.122" 
-$SubnetMask = "255.255.255.0"
-$DefaultGateway = "192.168.1.1" 
-$DNSServer = "192.168.1.1"  
-Set-NetAdapterBinding -Name $InterfaceAlias -ComponentID ms_tcpip6 -Enabled $false
-Write-Host "Configuration de l'adresse IP..."
-New-NetIPAddress -InterfaceAlias $InterfaceAlias -IPAddress $IPAddress -PrefixLength $PrefixLength -DefaultGateway $DefaultGateway -Confirm:$false
-Write-Host "Configuration du serveur DNS..."
-Set-DnsClientServerAddress -InterfaceAlias $InterfaceAlias -ServerAddresses $DNSServer
+t connect
 
 
 
 netsh winhttp set proxy "192.168.151.254:3128"
 netsh winhttp set proxy "192.168.151.254:3128" bypass-list="192.168.*"
 netsh winhttp import proxy source=ie
+https://www.it-connect.fr/comment-utiliser-powershell-derriere-un-proxy/
+
+
+
+Pas it connect
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer -Value "192.168.151.254:3128"
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable -Value 1
+
+https://www.techcrafters.com/portal/en/kb/articles/powershell-comprehensive-guide-to-configuring-proxy-settings-on-windows#Disabling_the_Proxy
