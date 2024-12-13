6eb32b70e11abd339256716109840d738c85c7d7
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-13 18:01:44 +0100

    Update promotheus.sh

--- script/projet/promotheus.sh
+++ script/projet/promotheus.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
-
-# Mettre à jour le système
-sudo apt update && sudo apt upgrade -y
-
-# Installer Prometheus
-sudo useradd --no-create-home --shell /bin/false prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.47.0/prometheus-2.47.0.linux-amd64.tar.gz
+tar vxf prometheus*.tar.gz
+sudo groupadd --system prometheus
+sudo useradd -s /sbin/nologin --system -g prometheus prometheus
 sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
-sudo chown prometheus:prometheus /etc/prometheus /var/lib/prometheus
-
-wget https://github.com/prometheus/prometheus/releases/download/v2.31.1/prometheus-2.31.1.linux-amd64.tar.gz
-tar -xvf prometheus-2.31.1.linux-amd64.tar.gz
-cd prometheus-2.31.1.linux-amd64
-sudo cp prometheus /usr/local/bin/
-sudo cp promtool /usr/local/bin/
-sudo cp -r consoles /etc/prometheus
-sudo cp -r console_libraries /etc/prometheus
-sudo cp prometheus.yml /etc/prometheus
-
-sudo chown -R prometheus:prometheus /etc/prometheus /usr/local/bin/prometheus /usr/local/bin/promtool
-
-# Créer le service systemd pour Prometheus
-echo "[Unit]
+cd prometheus*/
+sudo mv prometheus /usr/local/bin
+sudo mv promtool /usr/local/bin
+sudo mv console* /etc/prometheus
+sudo mv prometheus.yml /etc/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo chown -R prometheus:prometheus /var/lib/prometheus
+sudo nano /etc/systemd/system/prometheus.service
+cat <<EOF
+[Unit]
 Description=Prometheus
 Wants=network-online.target
 After=network-online.target
@@ -30,12 +27,15 @@ After=network-online.target
 User=prometheus
 Group=prometheus
 Type=simple
-ExecStart=/usr/local/bin/prometheus --config.file /etc/prometheus/prometheus.yml --storage.tsdb.path /var/lib/prometheus/
+ExecStart=/usr/local/bin/prometheus \
+ --config.file /etc/prometheus/prometheus.yml \
+ --storage.tsdb.path /var/lib/prometheus/ \
+ --web.console.templates=/etc/prometheus/consoles \
+ --web.console.libraries=/etc/prometheus/console_libraries
 
 [Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/prometheus.service
-
-# Démarrer et activer Prometheus
+WantedBy=multi-user.target
+EOF
 sudo systemctl daemon-reload
-sudo systemctl start prometheus
 sudo systemctl enable prometheus
+sudo systemctl start prometheus
