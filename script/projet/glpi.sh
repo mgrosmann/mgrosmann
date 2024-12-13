ea21db104feee87b71d6f5880e36b74b1452ee82
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-13 20:52:05 +0100

    Update glpi.sh

--- script/projet/glpi.sh
+++ script/projet/glpi.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
-apt-get update && apt-get upgrade -y
-apt-get install -y apache2  php php-mysql php-xml php-mbstring php-curl php-gd php-intl php-ldap php-apcu
-mysql <<EOF
+apt install -y apache2 php php-{apcu,cli,common,curl,gd,imap,ldap,mysql,xmlrpc,xml,mbstring,bcmath,intl,zip,redis,bz2} libapache2-mod-php php-soap php-cas
 CREATE DATABASE glpi;
-CREATE USER 'glpi'@'localhost' IDENTIFIED BY 'glpi';
-GRANT ALL PRIVILEGES ON glpi.* TO 'glpi'@'localhost';
+GRANT ALL PRIVILEGES ON dglpi.* TO glpi@localhost IDENTIFIED BY "glpi";
 FLUSH PRIVILEGES;
-EXIT;
+mysql -uroot -pmysql <<EOF
+CREATE DATABASE glpi;
+CREATE USER 'glpi'@'localhost' IDENTIFIED BY 'yourstrongpassword';
+GRANT ALL PRIVILEGES ON glpi.* TO 'glpi'@'localhost';
+GRANT SELECT ON `mysql`.`time_zone_name` TO 'glpi'@'localhost';
+FLUSH PRIVILEGES;
 EOF
-wget https://github.com/glpi-project/glpi/releases/download/10.0.17/glpi-10.0.17.tgz
-tar -xvzf glpi-10.0.17.tgz
-mv glpi /var/www/html/
-chown -R www-data:www-data /var/www/html/glpi
-chmod -R 755 /var/www/html/glpi
-systemctl restart apache2
+cd /var/www/html
+wget https://github.com/glpi-project/glpi/releases/download/10.0.15/glpi-10.0.15.tgz
+tar -xvzf glpi-10.0.15.tgz
+chown root:root /var/www/html/glpi/ -R
+chown www-data:www-data /etc/glpi -R
+chown www-data:www-data /var/lib/glpi -R
+chown www-data:www-data /var/log/glpi -R
+chown www-data:www-data /var/www/html/glpi/marketplace -Rf
+cat <<EOF > glpi.conf
+# Start of the VirtualHost configuration for port 80
+
+<VirtualHost *:80>
+    ServerName yourglpi.yourdomain.com
+    # Specify the server's hostname
+    DocumentRoot /var/www/html/glpi/public
+    # The directory where the website's files are located
+    # Start of a Directory directive for the website's directory
+    <Directory /var/www/html/glpi/public>
+        Require all granted
+        # Allow all access to this directory
+        RewriteEngine On
+        # Enable the Apache rewrite engine
+        # Ensure authorization headers are passed to PHP.
+        # Some Apache configurations may filter them and break usage of API, CalDAV, ...
+        RewriteCond %{HTTP:Authorization} ^(.+)$
+        RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+        # Redirect all requests to GLPI router, unless the file exists.
+        RewriteCond %{REQUEST_FILENAME} !-f
+        RewriteRule ^(.*)$ index.php [QSA,L]
+    </Directory>
+    # End of the Directory directive for /var/www/glpi/public
+</VirtualHost>
+
+# End of the VirtualHost configuration for port 80
+EOF
