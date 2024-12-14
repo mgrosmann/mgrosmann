ad6acb6249a68cdfab54bfa9ba154de2fc5d2e69
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-14 10:37:59 +0100

    Update rsync.sh

--- script/projet/rsync.sh
+++ script/projet/rsync.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y rsync
+mkdir -p /home/mgrosmann/backup
+mkdir -p /home/mgrosmann/desktop
+mkdir -p /home/mgrosmann/save
+wget https://mgrosmann.onrender.com/script/save.sh
+wget https://mgrosmann.onrender.com/script/backup.sh
+chmod +x save.sh
+chmod +x backup.sh
