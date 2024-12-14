7c52654b195dd83657a6c1c53aa3827165f62fca
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2024-12-14 10:59:13 +0100

    Update backup.sh

--- script/projet/backup.sh
+++ script/projet/backup.sh
@@ -5,4 +5,3 @@ REMOTE_USER="mgrosmann"
 REMOTE_HOST="192.168.1.45"
 REMOTE_DIR="/home/mgrosmann/save"
 rsync -avz $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR $BACKUP_DIR
-echo "La sauvegarde et la restauration avec rsync ont été effectuées avec succès."
