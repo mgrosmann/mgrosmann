4194e4f4e54a1b1dc1c64ad2e0eccd6504ba2d85
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-11 15:55:16 +0100

    test

---
+++ script/projet/veeam.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://repository.veeam.com/backup/linux/agent/dpkg/debian/public/pool/veeam/v/veeam-release-deb/veeam-release-deb_1.0.9_amd64.deb
+apt-get install veeam-release-deb_1.0.9_amd64.deb
+apt-get install veeam
