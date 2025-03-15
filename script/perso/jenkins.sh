e2ba012792c4237cf2ab397e632c297250dc8d67
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-15 21:14:29 +0100

    jenkins

--- script/perso/jenkins.sh
+++ script/perso/jenkins.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+sudo apt update
+sudo apt install default-jdk -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+echo "allez sur 'http://localhost:8080'"
