8130cab5eb78d7e0735bfdaa6ae8595c08f61024
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-15 21:02:52 +0100

    s

--- script/autres/proxy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export http_proxy="http://192.168.151.254:3128"
-export https_proxy="http://192.168.151.254:3128"
--- script/autres/remove.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-wget https://mgrosmann.onrender.com/script/fvcknextcloud.sh
-wget https://mgrosmann.onrender.com/script/nikphp.sh
-wget https://mgrosmann.onrender.com/script/uninstall.sh
-chmod +x *.sh
-bash fvcknextcloud.sh
-bash nikphp.sh
-bash uninstall.sh
