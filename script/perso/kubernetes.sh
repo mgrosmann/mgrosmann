51c3841b6e8fb70a8e2d522719640c48356a05b0
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-16 06:41:12 +0100

    k

--- script/perso/kubernetes.sh
+++ script/perso/kubernetes.sh
@@ -9,12 +9,38 @@ then
 else
     echo "docker est déjà installé."
 fi
+apt install curl
+apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+install -y kubelet kubeadm kubectl
+hostnamectl set-hostname "master-node"
+exec bash
+hostnamectl set-hostname "w-node1"
+exec bash
+cat <<EOF>> /etc/hosts
+10.168.10.207 master-node
+10.168.10.208 node1 W-node1
+10.168.10.209 node2 W-node2
+EOF
+ufw allow 6443/tcp
+ufw allow 2379/tcp
+ufw allow 2380/tcp
+ufw allow 10250/tcp
+ufw allow 10251/tcp
+ufw allow 10252/tcp
+ufw allow 10255/tcp
+ufw reload
+ufw allow 10251/tcp
+ufw allow 10255/tcp
+ufw reload
 swapoff -a
-sed -i '/ swap / s/^/#/' /etc/fstab
-apt install -y kubelet kubeadm kubectl
 systemctl enable kubelet
-kubeadm init --pod-network-cidr=<your-pod-network-cidr>
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
+
+
+
+
