7a5da4eccd8ac40c2f7d00ef355ea9481d45bbae
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-17 10:49:34 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+wget https://mgrosmann.onrender.com/script/cours/proxy.sh
+chmod +x proxy.sh
+bash proxy.sh
 git clone https://github.com/mgrosmann/mgrosmann.git
 git clone https://github.com/mgrosmann/docker.git
