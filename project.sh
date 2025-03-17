1773bf57dafd730fabde7b425fe6a6e60887ef64
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-17 11:17:33 +0100

    Create project.sh

---
+++ project.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://mgrosmann.onrender.com/script/projet/proxy.sh
+chmod +x proxy.sh
+bash proxy.sh
+git clone https://github.com/mgrosmann/mgrosmann.git
+git clone https://github.com/mgrosmann/docker.git
