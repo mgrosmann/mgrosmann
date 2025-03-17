8988222d498cc0fb75adb9664472c4559e893d74
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-17 17:10:08 +0100

    Update proxy.sh

--- script/cours/proxy.sh
+++ script/cours/proxy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo 'Acquire::http::proxy  "http://182.168.182.254:3128/";' > /etc/apt/apt.conf
-echo 'Acquire::https::proxy "http://182.168.182.254:3128/";' >> /etc/apt/apt.conf
-export http_proxy="http://182.168.182.254:3128"
-export https_proxy="http://182.168.182.254:3128"
-git config --global http.proxy http://182.168.182.254:3128
-git config --global https.proxy http://182.168.182.254:3128
-mkdir -p /etc/systemd/system/docker.service.d
-echo "[Service]" >> /etc/systemd/system/docker.service.d/http-proxy.conf
-echo "Environment="HTTP_PROXY=http://182.168.152.254:3128/" "HTTPS_PROXY=http://182.168.152.254:3128/"" >> /etc/systemd/system/docker.service.d/http-proxy.conf
+echo 'Acquire::http::proxy  "http://192.168.151.254:3128/";' > /etc/apt/apt.conf
+echo 'Acquire::https::proxy "http://192.168.151.254:3128/";' >> /etc/apt/apt.conf
+echo "https_proxy = http://192.168.151.254:3128/" >> /etc/wgetrc
+echo "http_proxy = http://192.168.151.254:3128/" >> /etc/wgetrc
+export http_proxy="http://192.168.151.254:3128"
+export https_proxy="http://192.168.151.254:3128"
+git config --global http.proxy http://192.168.151.254:3128
+git config --global https.proxy http://192.168.151.254:3128
+#mkdir -p /etc/systemd/system/docker.service.d
+#echo "[Service]" >> /etc/systemd/system/docker.service.d/http-proxy.conf
+#echo "Environment="HTTP_PROXY=http://192.168.151.254:3128/" "HTTPS_PROXY=http://192.168.151.254:3128/"" >> /etc/systemd/system/docker.service.d/http-proxy.conf
