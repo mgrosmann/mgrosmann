4753d38e82e468dad207d057f1b9ce527f633bfb
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-17 10:53:17 +0100

    Create proxy.sh

---
+++ script/cours/proxy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo 'Acquire::http::proxy  "http://182.168.182.254:3128/";' > /etc/apt/apt.conf
+echo 'Acquire::https::proxy "http://182.168.182.254:3128/";' >> /etc/apt/apt.conf
+export http_proxy="http://182.168.182.254:3128"
+export https_proxy="http://182.168.182.254:3128"
+git config --global http.proxy http://182.168.182.254:3128
+git config --global https.proxy http://182.168.182.254:3128
+mkdir -p /etc/systemd/system/docker.service.d
+echo "[Service]" >> /etc/systemd/system/docker.service.d/http-proxy.conf
+echo "Environment="HTTP_PROXY=http://182.168.152.254:3128/" "HTTPS_PROXY=http://182.168.152.254:3128/"" >> /etc/systemd/system/docker.service.d/http-proxy.conf
