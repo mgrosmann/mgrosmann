0f80ad76b668814913f114dece975609e2288384
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-18 11:04:28 +0100

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 wget https://mgrosmann.onrender.com/script/projet/proxy.sh
 chmod +x proxy.sh
-bash proxy.sh
 git clone https://github.com/mgrosmann/mgrosmann.git
 git clone https://github.com/mgrosmann/docker.git
