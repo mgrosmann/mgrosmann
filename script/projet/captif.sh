0b58e782fc3f1a6ddf001dbce6dabafcb2c1da95
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-19 08:38:00 +0100

    Update captif.sh

--- script/projet/captif.sh
+++ script/projet/captif.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-read -p "entrer l'url: " url
-read -p "entrer le user: " user
-read -s -p "entrer le password: " passwd 
-curl --request POST --data "user=$user&cmd=authenticate&password=$passwd" ''$url''
+curl --request POST --data "user=tbm&cmd=authenticate&password=P@ssw0rd" 'https://192.168.1.1/index.php?zone=portail'
