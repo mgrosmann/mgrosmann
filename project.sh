428c9668d17e6253d2e39a7c80dd7e3baca74a47
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-19 08:36:35 +0100

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 wget https://mgrosmann.onrender.com/script/projet/proxy.sh
 chmod +x proxy.sh
+bash proxy.sh
+wget https://mgrosmann.onrender.com/script/projet/captif.sh
+chmod +x captif.sh
+bash captif.sh
 git clone https://github.com/mgrosmann/mgrosmann.git
 git clone https://github.com/mgrosmann/docker.git
