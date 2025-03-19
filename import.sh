258ab7dc994df1a0c2d5f3e04b57fa36976639e6
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-19 14:03:47 +0100

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-read -p "fichier a exporter: " file
-scp mgrosmann@192.168.154.3:/home/SIO/mgrosmann/$file
+read -p "fichier a importer: " file
+scp mgrosmann@192.168.154.3:/home/SIO/mgrosmann/$file /root
