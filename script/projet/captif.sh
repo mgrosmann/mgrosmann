9f9e50c9b5cc4e8346fe7a1d84bc30bc9d56126c
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-19 08:40:17 +0100

    Update captif.sh

--- script/projet/captif.sh
+++ script/projet/captif.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --request POST --data "user=tbm&cmd=authenticate&password=P@ssw0rd" 'https://192.168.1.1/index.php?zone=portail'
+curl --request POST --data "user=tbm&cmd=authenticate&password=P@ssw0rd" 'http://192.168.1.1:8002/index.php?zone=portail'
