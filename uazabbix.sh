eb06b05282f063c6cd13dd584d55e9d068b1f6af
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-20 11:32:08 +0100

    Create uazabbix.sh

---
+++ uazabbix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://repo.zabbix.com/zabbix/7.2/release/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.2+ubuntu24.04_all.deb
+dpkg -i zabbix-release_latest_7.2+ubuntu24.04_all.deb
+apt update
+apt install zabbix-agent
+systemctl restart zabbix-agent
+systemctl enable zabbix-agent
