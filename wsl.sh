040d3f4408ff577ca077b388341a8ce0e04f1c84
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-23 18:30:32 +0100

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -4,16 +4,14 @@ ln -s /mnt/c/Users/PC/Downloads /root/telechargements
 ln -s /mnt/c/Users/PC/Videos /root/videos
 apt update
 apt install apache2 libapache2-mod-php ssh git zip -y
-rm -rf /var/www/html/
-cp -r /root/doc/htdocs /var/www/html
-cd /root
 wget mgrosmann.vercel.app/script/perso/jump.sh
 wget mgrosmann.vercel.app/script/perso/sio_jump.sh
 wget mgrosmann.vercel.app/autres/html.zip
 wget mgrosmann.vercel.app/autres/html.z01
 wget mgrosmann.vercel.app/autres/html.z02
-unzip html.* -d /var/www
-rm html.zip
+zip -FF html.zip --out html_complete.zip
+unzip html_complete.zip -d /var/www
+rm html*
 git clone https://github.com/mgrosmann/docker.git
 git clone https://github.com/mgrosmann/mgrosmann.git
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
