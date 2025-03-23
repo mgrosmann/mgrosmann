492125ab5fde603063f212832588077d3075f264
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-23 10:07:25 +0100

    Delete chrome.ps1

--- chrome.ps1
+++
@@ -1,4 +0,0 @@
-$InstallerPath = "$env:TEMP\\chrome_installer.exe"
-Invoke-WebRequest -Uri "https://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $InstallerPath
-Start-Process -FilePath $InstallerPath -ArgumentList "/silent /install" -Wait
-Remove-Item -Path $InstallerPath
