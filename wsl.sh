b8057dab6332e6db6cc9e922c80fb90792bbd8f7
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-23 10:17:02 +0100

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -3,7 +3,7 @@ ln -s /mnt/c/Users/PC/Documents /root/doc
 ln -s /mnt/c/Users/PC/Downloads /root/telechargements
 ln -s /mnt/c/Users/PC/Videos /root/videos
 apt update
-apt install apache2 -y
+apt install apache2 libapache2-mod-php -y
 rm -rf /var/www/html/
 cp -r /root/doc/htdocs /var/www/html
 cd /root
