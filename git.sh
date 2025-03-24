195891ab44d3500757297c865ec28226e0422bf2
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-24 12:29:58 +0100

    Update git.sh

--- git.sh
+++ git.sh
@@ -4,5 +4,5 @@ git config --global user.email "grosmannmatheo@gmail.com"
 git config --global url."git@github.com:".insteadOf "https://github.com/"
 git add .
 git commit -m "fichier importé par ssh en linux"
-git push origin main || { echo "Échec du push."; exit 1; }
-echo "Fichiers envoyés avec succès sur GitHub via SSH ! ✅"
+git push origin main
+echo "Fichiers envoyés avec succès sur GitHub via SSH"
