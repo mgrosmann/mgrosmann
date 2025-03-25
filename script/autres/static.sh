429f3c0b2cf67459d059ad024050f442035e84f9
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-25 20:35:32 +0100

    Update static.sh

--- script/autres/static.sh
+++ script/autres/static.sh
@@ -3,7 +3,6 @@ read -p "Entrer l'interface réseau: " interface
 read -p "Entrer l'adresse IP (au format CIDR, ex: 192.168.1.10/24): " ip
 read -p "Entrer la passerelle: " gateway
 read -p "Entrer le DNS: " dns
-
 cat <<EOF > /etc/netplan/50-cloud-init.yaml
 network:
   version: 2
