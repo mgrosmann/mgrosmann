d652a917200cc09120941a66624736fa61b674ae
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-30 20:32:15 +0200

    Update zabbix.bat

--- script/projet/zabbix.bat
+++ script/projet/zabbix.bat
@@ -1,9 +1,12 @@
-@echo off
-Set "GLPI=\\192.168.1.10\Partage\GLPI-Agent-1.7.3-x64.msi"
-msiexec /l*v log.txt /i zabbix_agent-4.0.6-x86.msi /qn^
-        SERVER=192.168.6.76^
-        TLSCONNECT=psk^
-        TLSACCEPT=psk^
-        TLSPSKIDENTITY=MyPSKID^
-        TLSPSKVALUE=1f87b595725ac58dd977beef14b97461a7c1045b9a1c963065002c5473194952
-pause
+@ECHO OFF
+Set "zabbix=\\192.168.1.10\Partage\GLPI-Agent-1.7.3-x64.msi"
+set /p server=IP du  Zabbix Server: 
+set /p hostname=Hostname du Zabbix Agent: 
+REM regle pare feu
+netsh advfirewall firewall add rule name="Zabbix Agent" dir=in action=allow protocol=TCP localport=10050
+msiexec /l*v %TEMP%\install-zabbix-agent-log.txt /i %zabbix%^
+ SERVER=%server%^
+ SERVERACTIVE=%server%^
+ HOSTNAME=%hostname%^
+ TIMEOUT=15^
+ /qn
