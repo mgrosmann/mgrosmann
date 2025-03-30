f796be1d8c94edc41931d493317ed8b6e981ffa3
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-30 21:40:07 +0200

    Delete script/projet/nfs_server.sh

--- script/projet/nfs_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update
-apt install nfs-kernel-server
-mkdir /mnt/NFS-BACKUP
-chown nobody:nogroup /mnt/NFS-BACKUP
-chmod 777 /mnt/NFS-BACKUP
-exportfs -a
-systemctl restart nfs-kernel-server
