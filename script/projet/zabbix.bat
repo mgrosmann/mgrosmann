539e5bd16789ad35a16fd46e1b68ef784eb600f4
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-30 21:20:37 +0200

    Update zabbix.bat

--- script/projet/zabbix.bat
+++ script/projet/zabbix.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 Set "zabbix=\\192.168.1.10\Partage\GLPI-Agent-1.7.3-x64.msi"
-set /p server=IP du  Zabbix Server: 
+set /p server=IP du Zabbix Server: 
 set /p hostname=Hostname du Zabbix Agent: 
 REM regle pare feu
 netsh advfirewall firewall add rule name="Zabbix Agent" dir=in action=allow protocol=TCP localport=10050
