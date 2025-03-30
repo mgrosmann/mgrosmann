dccba8316ca526fcb13031e1165b004301ee7e25
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-30 21:50:46 +0200

    Update agent-glpi.sh

--- script/projet/agent-glpi.sh
+++ script/projet/agent-glpi.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-echo -e "\e[33mAssurez-vous que le serveur GLPI est opérationnel et que l'installation sur l'interface web est finalisée.\e[0m"
+echo -e "\e[33mAssurez-vous que le serveur GLPI est opérationnel, que l'installation sur l'interface web a été finalisée et que l'inventaire a été activé.\e[0m"
 read -p "Le serveur GLPI est-il prêt ? (y/n): " confirm
-if [[ $confirm != "y" ]]; then
+if [[ $confirm = "y" ]]; then
+    read -p "veuillez entrez l'adresse ip du serveur glpi" server
+    apt install perl
+    wget https://github.com/glpi-project/glpi-agent/releases/download/1.7.1/glpi-agent-1.7.1-linux-installer.pl
+    perl glpi-agent-1.7.1-linux-installer.pl -s $server/glpi --runnow --install
+    systemctl enable glpi-agent
+elif[[ $confirm = "n" ]]; then
     echo "Veuillez finaliser l'installation du serveur GLPI avant de continuer."
-    exit 1
+else
+    echo "choix non pris en compte"
 fi
-apt install perl
-wget https://github.com/glpi-project/glpi-agent/releases/download/1.7.1/glpi-agent-1.7.1-linux-installer.pl
-perl glpi-agent-1.7.1-linux-installer.pl -s http://192.168.1.78/glpi --runnow --install
-systemctl enable glpi-agent
