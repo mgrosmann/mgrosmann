023998605d0b0489f76a697ac2cf89b938efd575
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-30 21:19:56 +0200

    Update and rename glpi.bat to glpi.bat

--- script/projet/glpi.bat
+++ script/projet/glpi.bat
@@ -1,4 +1,5 @@
 @echo off
+if not exist "C:\Program Files\GLPI-Agent"
 Set "GLPI=\\192.168.1.10\Partage\GLPI-Agent-1.7.3-x64.msi"
 Msiexec /i "%GLPI%" /quiet SERVER=http://192.168.1.11/glpi ADD_FIREWALL_EXCEPTION=1
 pause
