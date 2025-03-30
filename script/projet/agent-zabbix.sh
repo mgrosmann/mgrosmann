2e6747a92953ba39c75cfd8c3543a87cfb408f2d
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-30 18:48:32 +0200

    Update and rename azabbix.sh to agent-zabbix.sh

---
+++ script/projet/agent-zabbix.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+read -p "quel distribution linux avez vous ? 1 pour debian 2 pour ubuntu" linux
+if [[ $linux == 1 ]]; then
+  wget https://repo.zabbix.com/zabbix/7.2/release/debian/pool/main/z/zabbix-release/zabbix-release_latest_7.2+debian12_all.deb
+  dpkg -i zabbix-release_latest_7.2+debian12_all.deb
+  apt update
+  apt install zabbix-agent
+  systemctl restart zabbix-agent
+  systemctl enable zabbix-agent
+elif [[ $linux == 2 ]]; then
+  wget https://repo.zabbix.com/zabbix/7.2/release/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.2+ubuntu24.04_all.deb
+  dpkg -i zabbix-release_latest_7.2+ubuntu24.04_all.deb
+  apt update
+  apt install zabbix-agent
+  systemctl restart zabbix-agent
+  systemctl enable zabbix-agent
+else
+  echo "choix incorrect seuls debian et ubuntu sont pris en compte"
+fi
--- script/projet/azabbix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://repo.zabbix.com/zabbix/7.2/release/debian/pool/main/z/zabbix-release/zabbix-release_latest_7.2+debian12_all.deb
-dpkg -i zabbix-release_latest_7.2+debian12_all.deb
-apt update
-apt install zabbix-agent
-systemctl restart zabbix-agent
-systemctl enable zabbix-agent
