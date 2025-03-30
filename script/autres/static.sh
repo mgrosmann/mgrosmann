e49eb55ee7a41612711a90e09c711f76a0860a02
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-30 21:34:18 +0200

    Update static.sh

--- script/autres/static.sh
+++ script/autres/static.sh
@@ -5,21 +5,24 @@ read -p "Entrer la passerelle: " gateway
 read -p "Entrer le DNS: " dns
 read -p "sur quel distri linux êtes vous ? (1 pour debian,2 pour ubntu) " linux
 if [[ $linux == 1 ]]; then
-  cat <<EOF > /etc/netplan/50-cloud-init.yaml
-  network:
-    version: 2
-    ethernets:
-      $interface:
-        addresses:
-          - $ip
-        routes:
-          - to: default
-            via: $gateway
-        nameservers:
-          addresses:
-            - $dns
+  cat <<EOF > /etc/network/interfaces
+  # This file describes the network interfaces available on your system
+  # and how to activate them. For more information, see interfaces(5).
+
+  source /etc/network/interfaces.d/*
+
+  # The loopback network interface  
+  auto lo
+  iface lo inet loopback
+
+  # The primary network interface
+  allow-hotplug $interface
+  iface $interface inet static
+      address $ip
+      gateway $gateway
+      dns-nameservers $dns
   EOF
-  sudo netplan apply
+  systemctl restart networking.service
   hostname -I
 if [[ $linux == 2 ]]; then
   cat <<EOF > /etc/netplan/50-cloud-init.yaml
