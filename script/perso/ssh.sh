5fee3172ce93cfbcfe26c4db72bb1135a8b2f135
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-31 12:41:21 +0200

    Update ssh.sh

--- script/perso/ssh.sh
+++ script/perso/ssh.sh
@@ -3,13 +3,13 @@ read -p "besoin du jump ssh ? (1 pour non 2 pour oui)" jump
 if [[ $jump == 1 ]]; then
     read -p "nom de l'utilisateur du serveur ssh distant" user
     read -p "hostname/Adresse ip du serveur ssh distant" host 
-    if [ ! -f ~/.ssh/id_rsa.pub   ]; then
+    if [ ! -f ~/.ssh/id_rsa.pub ]; then
         ssh-keygen -t rsa
         ssh-copy-id -i ~/.ssh/id_rsa.pub $user@$host
     elif
         ssh-copy-id -i ~/.ssh/id_rsa.pub $user@$host
 elif [[ $jump == 2 ]]; then
-    if [ ! -f ~/.ssh/id_rsa.pub   ]; then
+    if [ ! -f ~/.ssh/id_rsa.pub ]; then
         ssh-keygen -t rsa
     fi
 read -p "besoin de combien de jump ssh ?" juump
@@ -17,5 +17,6 @@ read -p "besoin de combien de jump ssh ?" juump
     do
     read -p "nom de l'utilisateur du serveur ssh distant numero $i" user_$i
     read -p "hostname/Adresse ip du serveur ssh distant $i" host_$i
-    ssh-copy-id -o ProxyJump=$user_$i @$host_$i 
-#echo "enter key public" > ~/.ssh/authorized_keys
+    echo "$user_$i@$host_$i" >> /root/host.txt
+echo "effectuer ssh -J user@host:port user@host avec les host presents dans /root/host.txt"
+#echo "your key public" > ~/.ssh/authorized_keys
