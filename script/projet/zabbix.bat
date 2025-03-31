e82f28ad9414ac4faeb79211c545e8f6f874479d
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-31 22:24:17 +0200

    Update zabbix.bat

--- script/projet/zabbix.bat
+++ script/projet/zabbix.bat
@@ -1,4 +1,8 @@
 @ECHO OFF
+if exist "C:\Program Files\Zabbix Agent" (
+    echo Zabbix Agent est déjà installé. Fin du script.
+    exit /b
+)
 set "zabbix=\\192.168.1.10\script$\zabbix_agent-7.2.5-windows-amd64-openssl.msi"
 set "server=http://192.168.1.11"
 set "hostname=zabbix-agent"
