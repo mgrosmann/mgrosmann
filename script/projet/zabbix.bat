c528e8a62bba34d5f46355274fbaf46408a7ed19
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-31 22:21:31 +0200

    Update zabbix.bat

--- script/projet/zabbix.bat
+++ script/projet/zabbix.bat
@@ -1,12 +1,6 @@
 @ECHO OFF
-Set "zabbix=\\192.168.1.10\Partage\GLPI-Agent-1.7.3-x64.msi"
-set /p server=IP du Zabbix Server: 
-set /p hostname=Hostname du Zabbix Agent: 
-REM regle pare feu
-netsh advfirewall firewall add rule name="Zabbix Agent" dir=in action=allow protocol=TCP localport=10050
-msiexec /l*v %TEMP%\install-zabbix-agent-log.txt /i %zabbix%^
- SERVER=%server%^
- SERVERACTIVE=%server%^
- HOSTNAME=%hostname%^
- TIMEOUT=15^
- /qn
+set "zabbix=\\192.168.1.10\script$\zabbix_agent-7.2.5-windows-amd64-openssl.msi"
+set "server=192.168.1.11"
+set "hostname=zabbix-agent"
+msiexec /i "%zabbix%" /qn SERVER=%server% SERVERACTIVE=%server% HOSTNAME=%hostname%
+pause
