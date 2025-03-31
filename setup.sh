edb8528014a7acdacc98fe9a0c0c1be7015da425
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-31 09:02:39 +0200

    rangement

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget https://mgrosmann.onrender.com/script/cours/proxy.sh
-chmod +x proxy.sh
-bash proxy.sh
-git clone https://github.com/mgrosmann/mgrosmann.git
-git clone https://github.com/mgrosmann/docker.git
