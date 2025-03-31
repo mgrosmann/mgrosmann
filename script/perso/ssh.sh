89269809abb00e814febf63ebb74e7ebe2ba592f
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-31 09:11:08 +0200

    d

--- script/perso/ssh.sh
+++ script/perso/ssh.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 read -p "nom de l'utilisateur du serveur ssh distant" user
-read -p "hostname/Adresse ip du serveur ssh distant" host
-FILE=~/.ssh/id_rsa.pub   
-if [ -f $FILE ]; then
+read -p "hostname/Adresse ip du serveur ssh distant" host 
+if [ ! -f ~/.ssh/id_rsa.pub   ]; then
+    ssh-keygen -t rsa
     ssh-copy-id -i ~/.ssh/id_rsa.pub $user@$host
 elif
-    ssh-keygen -t rsa
     ssh-copy-id -i ~/.ssh/id_rsa.pub $user@$host
 #ssh-copy-id -o ProxyJump=jumpuser@jumphost:2455 remoteuser@remotehost
 #echo "enter key public" > /root/.ssh/authorized_keys
