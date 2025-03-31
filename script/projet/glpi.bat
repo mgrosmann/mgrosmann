81255a418403a18085bec36bfd492e9b1c181e9e
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-03-31 22:24:21 +0200

    Update glpi.bat

--- script/projet/glpi.bat
+++ script/projet/glpi.bat
@@ -1,5 +1,8 @@
 @echo off
-if not exist "C:\Program Files\GLPI-Agent"
-Set "GLPI=\\192.168.1.10\Partage\GLPI-Agent-1.7.3-x64.msi"
-Msiexec /i "%GLPI%" /quiet SERVER=http://192.168.1.11/glpi ADD_FIREWALL_EXCEPTION=1
+if exist "C:\Program Files\GLPI-agent" (
+    echo Zabbix Agent est déjà installé. Fin du script.
+    exit /b
+)
+set "GLPI=\\192.168.1.10\script$\GLPI-Agent-1.7.3-x64.msi"
+msiexec /i "%GLPI%" /quiet SERVER="http://192.168.1.11/glpi" ADD_FIREWALL_EXCEPTION=1 
 pause
