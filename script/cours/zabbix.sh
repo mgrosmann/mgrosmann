40024f5a3e057ce942ec59030d93c95631fea1ba
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-01 08:12:29 +0200

    Update zabbix.sh

--- script/cours/zabbix.sh
+++ script/cours/zabbix.sh
@@ -10,18 +10,18 @@ elif [[ $linux == 1 ]]; then
 else
   echo "choix incorrect, veuilez réessayer"
 fi
-  apt update
-  apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent -y
-  echo "create database IF NOT EXISTS zabbix character set utf8mb4 collate utf8mb4_bin;" > zbx.sql
-  echo "create user IF NOT EXISTS zabbix@localhost identified by 'zabbix';" >> zbx.sql
-  echo "grant all privileges on zabbix.* to zabbix@localhost;" >> zbx.sql
-  echo "set global log_bin_trust_function_creators = 1;" >> zbx.sql
-  echo "flush privileges;" >> zbx.sql
-  mysql -uroot -p$pass < zbx.sql
-  zcat /usr/share/zabbix/sql-scripts/mysql/server.sql.gz > zbx.sql
-  echo "set global log_bin_trust_function_creators = 0;" >> zbx.sql
-  mysql -uroot -p$pass zabbix < zbx.sql
-  rm zbx.sql
-  echo "systemctl restart zabbix-server zabbix-agent apache2" > zbx.sh
-  echo "systemctl enable zabbix-server zabbix-agent apache2" >> zbx.sh
-  echo "nano /etc/zabbix/zabbix_server.conf pour DBPASSWORd, une fois fait, 'bash zbx.sh'"
+apt update
+apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent -y
+echo "create database IF NOT EXISTS zabbix character set utf8mb4 collate utf8mb4_bin;" > zbx.sql
+echo "create user IF NOT EXISTS zabbix@localhost identified by 'zabbix';" >> zbx.sql
+echo "grant all privileges on zabbix.* to zabbix@localhost;" >> zbx.sql
+echo "set global log_bin_trust_function_creators = 1;" >> zbx.sql
+echo "flush privileges;" >> zbx.sql
+mysql -uroot -p$pass < zbx.sql
+zcat /usr/share/zabbix/sql-scripts/mysql/server.sql.gz > zbx.sql
+echo "set global log_bin_trust_function_creators = 0;" >> zbx.sql
+mysql -uroot -p$pass zabbix < zbx.sql
+rm zbx.sql
+echo "systemctl restart zabbix-server zabbix-agent apache2" > zbx.sh
+echo "systemctl enable zabbix-server zabbix-agent apache2" >> zbx.sh
+echo "nano /etc/zabbix/zabbix_server.conf pour DBPASSWORd, une fois fait, 'bash zbx.sh'"
