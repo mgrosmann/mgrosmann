9cc0aeebe53c744f9d6194c885cbac651a488ff5
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-01 09:20:55 +0200

    Update glpi.bat

--- script/projet/glpi.bat
+++ script/projet/glpi.bat
@@ -1,7 +1,6 @@
 @echo off
 if exist "C:\Program Files\GLPI-AGENT\" (
     exit
-    pause
 )
 set "GLPI=\\192.168.1.10\script$\GLPI-Agent-1.7.3-x64.msi"
 msiexec /i "%GLPI%" /quiet SERVER="http://192.168.1.11/glpi" ADD_FIREWALL_EXCEPTION=1 
