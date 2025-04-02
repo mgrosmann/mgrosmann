a738dd567a846c370db1967d92d6dd29919dc30d
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-02 09:15:03 +0200

    Update zabbix.bat

--- script/projet/zabbix.bat
+++ script/projet/zabbix.bat
@@ -1,9 +1,8 @@
-@echo off
+@ECHO OFF
 if exist "C:\Program Files\zabbix agent\" (
-    exit
+exit
 )
-set "zabbix=\\192.168.1.10\script$\zabbix_agent-7.2.5-windows-amd64-openssl.msi"
-set "server=192.168.1.11"
-set "hostname=zabbix-agent"
-msiexec /i "%zabbix%" /qn SERVER=%server% SERVERACTIVE=%server% HOSTNAME=%hostname%
-pause
+set zabbix="\\192.168.1.10\script$\zabbix_agent-7.2.5-windows-amd64-openssl.msi"
+set server="192.168.1.11"
+set hostname="zabbix-agent"
+msiexec /i %zabbix% /qn SERVER=%server% SERVERACTIVE=%server% HOSTNAME=%hostname%
