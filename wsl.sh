327deb5837c793f604d2f3b1524bce9b7e9428c1
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-03 20:43:27 +0200

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 mkdir -p /root/bin
+echo "#!/bin/bash" > /root/bin/hello-world
+echo "echo 'hello world !!!'" >> /root/bin/hello-world
+wget mgrosmann.vercel.app/script/perso/jump.sh
+wget mgrosmann.vercel.app/script/perso/david.sh
+mv jump.sh /root/bin/jump
+mv david.sh /root/bin/david
+chmod +x /root/bin/*
 echo "export PATH=$PATH:/root/bin" >> /root/.bashrc
 source .bashrc
 ln -s /mnt/c/Users/PC/Documents /root/doc
@@ -9,9 +16,7 @@ apt update
 apt install apache2 php-mysql libapache2-mod-php ssh git zip -y
 wget mgrosmann.vercel.app/script/projet/mysql.sh
 bash mysql.sh
-bash /root/doc/mysql/save/sio.sh
-wget mgrosmann.vercel.app/script/perso/jump.sh
-wget mgrosmann.vercel.app/script/perso/sio_jump.sh
+mysql -uroot -p$pass < /root/doc/all-databases-202503311246.sql
 wget mgrosmann.vercel.app/autres/html.zip
 wget mgrosmann.vercel.app/autres/html.z01
 wget mgrosmann.vercel.app/autres/html.z02
@@ -20,6 +25,7 @@ unzip html_complete.zip -d /var/www
 rm html*
 git clone https://github.com/mgrosmann/docker.git
 git clone https://github.com/mgrosmann/mgrosmann.git
+cp -r mgrosmann /var/www/html/portfolio
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 ssh-copy-id -i ~/.ssh/id_rsa.pub -p1622 mgrosmann@sio.jbdelasalle.com
 ssh-copy-id -i ~/.ssh/id_rsa.pub -o ProxyJump=mgrosmann@sio.jbdelasalle.com:1622 root@192.168.182.1
