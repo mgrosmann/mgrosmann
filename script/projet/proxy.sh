daf843ce2d741853101bac62c61ab1f3c48fe809
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-06 10:12:44 +0200

    Update proxy.sh

--- script/projet/proxy.sh
+++ script/projet/proxy.sh
@@ -7,6 +7,8 @@ export http_proxy="http://192.168.182.254:3128"
 export https_proxy="http://192.168.182.254:3128"
 git config --global http.proxy http://192.168.182.254:3128
 git config --global https.proxy http://192.168.182.254:3128
-#mkdir -p /etc/systemd/system/docker.service.d
-#echo "[Service]" >> /etc/systemd/system/docker.service.d/http-proxy.conf
-#echo 'Environment="HTTP_PROXY=http://192.168.182.254:3128/" "HTTPS_PROXY=http://192.168.182.254:3128/"' >> /etc/systemd/system/docker.service.d/http-proxy.conf
+if command -v docker &> /dev/null; then
+  mkdir -p /etc/systemd/system/docker.service.d
+  echo "[Service]" >> /etc/systemd/system/docker.service.d/http-proxy.conf
+  echo 'Environment="HTTP_PROXY=http://192.168.182.254:3128/" "HTTPS_PROXY=http://192.168.182.254:3128/"' >> /etc/systemd/system/docker.service.d/http-proxy.conf
+fi
