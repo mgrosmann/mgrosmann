6e39dc1f66b6221f5413d39b8336f1c873c139d4
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-11 22:37:10 +0200

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -10,6 +10,10 @@ ln -s /mnt/c/Users/PC/Downloads /root/telechargements
 ln -s /mnt/c/Users/PC/Videos /root/videos
 apt update -y
 apt install apache2 php-mysql libapache2-mod-php ssh git zip python3 -y
+a2enmod userdir
+a2enmod rewrite
+systemctl restart apache2
+chmod -R 755 /home/
 wget -P /tmp/ mgrosmann.vercel.app/script/projet/mysql.sh
 bash /tmp/mysql.sh
 wget -P /tmp/ mgrosmann.vercel.app/autres/dump.sql
