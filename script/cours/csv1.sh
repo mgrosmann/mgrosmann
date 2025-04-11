ac164feeb9c52c8dec71234a3e6c4d93aad6e3ab
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-11 22:10:27 +0200

    Update csv1.sh

--- script/cours/csv1.sh
+++ script/cours/csv1.sh
@@ -8,9 +8,7 @@ if [[ ! -z $reponse && $reponse == "1" ]]; then
 fi
 mkdir -p $tmp
 mkdir -p $home
-#echo "Récupération des données des sites"
 wget -P $tmp https://sio.jbdelasalle.com/~bdufour/TP/yankrider_data/$user
-#echo "Création de l'annuaire des utilisateurs"
 echo "<!DOCTYPE html>
 <html lang='fr'>
 	<head>
@@ -22,16 +20,11 @@ echo "<!DOCTYPE html>
 while IFS=";" read -r id first_name last_name gender email function hiring_date	site; do
 	path_user_home=$home/$email
 	path_user_public_html=$path_user_home"/public_html"
-	#echo "Création du compte utilisateur pour "$first_name" "$last_name
 	useradd $email -m -d $path_user_home
-	#echo "Modification du mot de passe de "$first_name" "$last_name" pour btssio"
 	echo $email:root | chpasswd
-	#echo "Ajout de "$first_name" "$last_name" dans les groupes "$state_code" et "$site
 	usermod -aG www-data,$email
-	#echo "Création de public_html pour "$first_name" "$last_name
 	mkdir -p $path_user_public_html
 	chmod -R 755 $path_user_home
-	#echo "Création de index.html pour "$first_name" "$last_name
 	echo "<!DOCTYPE html>
 	<html lang='fr'>
 		<head>
@@ -43,9 +36,7 @@ while IFS=";" read -r id first_name last_name gender email function hiring_date	
 			"$first_name" "$last_name"
 		</body>
 	</html>" > $path_user_public_html"/index.html"
-	#echo "Ajout de "$first_name" "$last_name" dans l'annuaire"
 	echo "<div><a href='~"$email"/index.html'>"$first_name" "$last_name"</a></div>" >> /var/www/html/user.html
-	#echo "Mise à jour des droits de index.html de "$first_name" "$last_name
 	chmod 755 $path_user_public_html"/index.html"
 done < <(tail -n +2 $tmp/$user)
 echo '</body>
