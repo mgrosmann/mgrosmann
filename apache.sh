5aa9c50cdd0d7b400322aa8b7df3b941e2723560
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-11 10:36:52 +0200

    Create apache.sh

---
+++ apache.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+read -p "quel nom pour votre user" user
+mkdir -p /home/html/
+useradd $user -m -d /home/html/$user
+mkdir /home/html/$user/perso_html
+cat << EOF > /etc/apache2/mods-available/userdir.conf
+<IfModule mod_userdir.c>
+	UserDir perso_html
+	UserDir disabled root
+	
+	<Directory /home/html/*/perso_html>
+		AllowOverride FileInfo AuthConfig Limit Indexes
+		Options MultiViews Indexes SymLinksIfOwnerMatch IncludesNoExec
+		Require method GET POST OPTIONS
+	</Directory>
+</IfModule>
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
+#path = 
+#a2enmod userdir puis systemctl restart apache2
+EOF
+a2enmod userdir
+a2enmod rewrite
+systemctl restart apache2
