9b785f62a721c12956cdb8b133e23b08ec7a31a3
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-11 11:51:55 +0200

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -6,23 +6,25 @@ mkdir /home/html/$user/perso_html
 chown -R $user /home/html/$user
 echo "coucou bienvenue sur le site de moi, $user" > /home/html/$user/perso_html/index.html
 if [[ ! -f /etc/apache2/mods-available/userdir.conf ]]; then
-cat << EOF > /etc/apache2/mods-available/userdir.conf
-<IfModule mod_userdir.c>
-	UserDir perso_html
-	UserDir disabled root
+	cat << EOF > /etc/apache2/mods-available/userdir.conf
+	<IfModule mod_userdir.c>
+		UserDir perso_html
+		UserDir disabled root
 	
-	<Directory /home/html/*/perso_html>
-		AllowOverride FileInfo AuthConfig Limit Indexes
-		Options MultiViews Indexes SymLinksIfOwnerMatch IncludesNoExec
-		Require method GET POST OPTIONS
-	</Directory>
-</IfModule>
-# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
-#path = 
-#a2enmod userdir puis systemctl restart apache2
-EOF
-fi
+		<Directory /home/html/*/perso_html>
+			AllowOverride FileInfo AuthConfig Limit Indexes
+			Options MultiViews Indexes SymLinksIfOwnerMatch IncludesNoExec
+			Require method GET POST OPTIONS
+		</Directory>
+	</IfModule>
+	# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
+	#path = 
+	#a2enmod userdir puis systemctl restart apache2
+	EOF
+echo "config userdir crée"
 a2enmod userdir
 a2enmod rewrite
 systemctl restart apache2
 echo "le 'systemctl restart apache2' a deja ete effectuer par le script, votre site est disponible sur http://localhost/~user"
+else
+	
