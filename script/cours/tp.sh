bf8ee5d1cf07507571ce1be85caf0812b45684e6
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-12 15:14:43 +0200

    Update tp.sh

--- script/cours/tp.sh
+++ script/cours/tp.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 apt update -y
 apt install wget apache2 -y
-echo "<IfModule mod_userdir.c>
-	UserDir public_html
-	UserDir disabled root
-	
-	<Directory /home/html/*/public_html>
-		AllowOverride FileInfo AuthConfig Limit Indexes
-		Options MultiViews Indexes SymLinksIfOwnerMatch IncludesNoExec
-		Require method GET POST OPTIONS
-	</Directory>
-</IfModule>" > /etc/apache2/mods-available/userdir.conf
+read -p "quel conf pour userdir ? (1 pour sans groupe, 2 pour avec): " conf
+if [[ ! -z $conf && $conf == "1" ]]; then
+echo"<IfModule mod_userdir.c>
+        UserDir public_html
+        UserDir disabled root
+        
+        <Directory /home/html/*/public_html>
+                AllowOverride FileInfo AuthConfig Limit Indexes
+                Options MultiViews Indexes SymLinksIfOwnerMatch IncludesNoExec
+                Require method GET POST OPTIONS
+        </Directory>
+     </IfModule>" > /etc/apache2/mods-available/userdir.conf
+elif [[ ! -z $conf && $conf == "2" ]]; then
+echo"<IfModule mod_userdir.c>
+        UserDir public_html
+        UserDir disabled root
+        
+        <Directory /home/html/*/*/*/public_html>
+                AllowOverride FileInfo AuthConfig Limit Indexes
+                Options MultiViews Indexes SymLinksIfOwnerMatch IncludesNoExec
+                Require method GET POST OPTIONS
+        </Directory>
+     </IfModule>" > /etc/apache2/mods-available/userdir.conf
 a2enmod rewrite
 a2enmod userdir
 systemctl restart apache2
