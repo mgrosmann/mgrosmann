4f0e54f0698acf8d91fa46c7ff5f7b93ec928592
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-12 15:25:48 +0200

    Update tp.sh

--- script/cours/tp.sh
+++ script/cours/tp.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 apt update -y
 apt install wget apache2 -y
-read -p "quel conf pour userdir ? (1 pour sans groupe, 2 pour avec): " conf
-if [[ ! -z $conf && $conf == "1" ]]; then
-echo"<IfModule mod_userdir.c>
+echo "<IfModule mod_userdir.c>
         UserDir public_html
         UserDir disabled root
-        
+
         <Directory /home/html/*/public_html>
                 AllowOverride FileInfo AuthConfig Limit Indexes
                 Options MultiViews Indexes SymLinksIfOwnerMatch IncludesNoExec
                 Require method GET POST OPTIONS
         </Directory>
-     </IfModule>" > /etc/apache2/mods-available/userdir.conf
-elif [[ ! -z $conf && $conf == "2" ]]; then
-echo"<IfModule mod_userdir.c>
+</IfModule>" > /tmp/conf1
+echo "<IfModule mod_userdir.c>
         UserDir public_html
         UserDir disabled root
-        
+
         <Directory /home/html/*/*/*/public_html>
                 AllowOverride FileInfo AuthConfig Limit Indexes
                 Options MultiViews Indexes SymLinksIfOwnerMatch IncludesNoExec
                 Require method GET POST OPTIONS
         </Directory>
-     </IfModule>" > /etc/apache2/mods-available/userdir.conf
+</IfModule>" > /tmp/conf2
+read -p "quel conf pour userdir ? (1 pour sans groupe, 2 pour avec): " conf
+if [[ ! -z $conf && $conf == "1" ]]; then
+        cat /tmp/conf1 > /etc/apache2/mods-available/userdir.conf
+elif [[ ! -z $conf && $conf == "2" ]]; then
+         cat /tmp/conf1 > /etc/apache2/mods-available/userdir.conf
+else
+        echo "choix non pris en compte"
+fi
 a2enmod rewrite
 a2enmod userdir
 systemctl restart apache2
