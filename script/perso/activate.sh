37fd3a01ed287399425c2c156f187202fb856fd8
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-12 12:16:34 +0200

    Delete script/perso/activate.sh

--- script/perso/activate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-read -p "indiquer le repertoire" repertory
-read -p "indiquer le nom du site" site
-echo "Alias /$site  $repertory > /etc/apache2/conf-available/${site}.conf
-a2enconf $folder
-systemctl restart apache2
-echo "conf activé ! veuillez essayer http://localhost/$site"
