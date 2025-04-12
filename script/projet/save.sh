94744b7497e4035537b783a7062d5bf7eedb5bbf
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-12 16:21:30 +0200

    Delete script/projet/save.sh

--- script/projet/save.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SOURCE_DIR="/home/mgrosmann/desktop"
-BACKUP_DIR="/home/mgrosmann/backup"
-REMOTE_USER="mgrosmann"
-REMOTE_HOST="192.168.1.45"
-REMOTE_DIR="/home/mgrosmann/save"
-rsync -avz $SOURCE_DIR $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR
