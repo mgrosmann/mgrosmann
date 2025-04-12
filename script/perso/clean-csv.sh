5d2ed32e9c70e5548360c4a6c7fefd80fcd10525
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-12 12:33:26 +0200

    Create clean-csv.sh

---
+++ script/perso/clean-csv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+csv="/mnt/c/Users/PC/Downloads/user.csv"
+home="/home/html"
+tail -n +2 $csv | while IFS=',' read -r id nom prenom; do #lit le csv ligne par ligne
+	userdel -f $yusername 2> /dev/null
+rm -rf /home/html/
+rm -rf /var/www/html/user.html
