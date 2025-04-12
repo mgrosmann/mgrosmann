4227820e38d587c47a6f165d26c35d6f1ebbef37
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-12 14:48:48 +0200

    Update clean-csv-group.sh

--- script/perso/clean-csv-group.sh
+++ script/perso/clean-csv-group.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 csv="/mnt/c/Users/PC/Downloads/test.csv"
 home="/home/html"
-while IFS=',' read -r id nom prenom; do #lit le csv ligne par ligne
+while IFS=',' read -r id nom prenom pays region; do #lit le csv ligne par ligne
 	groupdel -f $pays 2> /dev/null
 	groupdel -f $region 2> /dev/null
 	userdel -f $yusername 2> /dev/null
