0e2e622da343f7653ef453404a8c27dbaae9ddb6
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-04-23 17:32:12 +0200

    Update veeam.sh

--- script/projet/veeam.sh
+++ script/projet/veeam.sh
@@ -2,5 +2,5 @@
 wget https://download2.veeam.com/VAL/v6/veeam-release-deb_1.0.9_amd64.deb
 dpkg -i ./veeam-release* && apt-get update
 apt-get install blksnap veeam -y
-apt-get installe veeam-nosnap -y
+apt-get install veeam-nosnap -y
 echo "effectuez 'veeamconfig ui' pour vérifier si l'installation a réussi"
