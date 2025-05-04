71964d2428450716ed157b2673e523b47da8806e
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-05-04 14:55:24 +0200

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -6,7 +6,7 @@ chmod +x /root/bin/*
 echo " PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> /root/.bashrc
 echo "export PATH=$PATH:/root/bin" >> /root/.bashrc
 ln -s /mnt/c/Users/PC/Documents /root/doc
-ln -s /mnt/c/Users/PC/Downloads /root/telechargements
+ln -s /mnt/c/Users/PC/Downloads /root/download
 ln -s /mnt/c/Users/PC/Videos /root/videos
 apt update -y
 apt install apache2 php-mysql libapache2-mod-php ssh git zip python3 -y
@@ -23,9 +23,8 @@ wget mgrosmann.vercel.app/autres/html.zip
 unzip html.zip -d /var/www/
 ln -s /var/www/html/ /root/web
 rm html*
-git clone https://github.com/mgrosmann/docker.git
 git clone https://github.com/mgrosmann/mgrosmann.git
-cp -r mgrosmann  /var/www/html//portfolio/
+mv mgrosmann  /var/www/html//portfolio/
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 #ssh-copy-id -i ~/.ssh/id_rsa.pub -p1622 mgrosmann@sio.jbdelasalle.com
 #ssh-copy-id -i ~/.ssh/id_rsa.pub -o ProxyJump=mgrosmann@sio.jbdelasalle.com:1622 root@192.168.182.1
