fda56600a6ea76ded47bfad25985a4a87a8c2059
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-05-07 08:35:34 +0200

    Update agent-glpi.sh

--- script/projet/agent-glpi.sh
+++ script/projet/agent-glpi.sh
@@ -5,7 +5,7 @@ if [[ $confirm = "y" ]]; then
     read -p "veuillez entrez l'adresse ip du serveur glpi" server
     apt install perl
     wget https://github.com/glpi-project/glpi-agent/releases/download/1.7.3/glpi-agent-1.7.3-linux-installer.pl
-    perl glpi-agent-1.7.3-linux-installer.pl -s $server/glpi --runnow --install
+    perl glpi-agent-1.7.3-linux-installer.pl -s http://$server/glpi --runnow --install
     systemctl enable glpi-agent
 elif [[ $confirm = "n" ]]; then
     echo "Veuillez finaliser l'installation du serveur GLPI avant de continuer."
