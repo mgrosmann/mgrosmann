be77861bf5d4a3e8f47a4ea30f495e3a5647f232
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-05-08 19:06:51 +0200

    Update glpi.sh

--- script/projet/glpi.sh
+++ script/projet/glpi.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+if ! command -v mysql &> /dev/null; then
+  wget mgrosmann.vercel.app/script/projet/mysql.sh
+  bash mysql.sh
+fi
+echo "quel est le mot de passe root du serveur sql ? "
+read -s pass
 apt update
 apt install -y apache2 php php-{apcu,cli,common,curl,gd,imap,ldap,mysql,xmlrpc,xml,mbstring,bcmath,intl,zip,redis,bz2} libapache2-mod-php php-soap php-cas
-mysql -uroot -proot   <<EOF
-CREATE USER 'glpi'@'localhost' IDENTIFIED BY 'glpi';
+echo "CREATE USER 'glpi'@'localhost' IDENTIFIED BY 'glpi';
 CREATE DATABASE glpi;
 GRANT ALL PRIVILEGES ON glpi.* TO 'glpi'@'localhost';
 GRANT SELECT ON \`mysql\`.\`time_zone_name\` TO 'glpi'@'localhost';
 FLUSH PRIVILEGES;
-EOF
+" > glpi.sql
+mysql -uroot -p$mdp < glpi.sql
 cd /var/www/html
 wget https://github.com/glpi-project/glpi/releases/download/10.0.17/glpi-10.0.17.tgz
 tar -xvzf glpi-10.0.17.tgz
