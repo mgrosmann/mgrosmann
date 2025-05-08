24113da28f273c49eec447c5ae8c7132ed31a7bb
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-05-08 19:06:48 +0200

    Update zabbix.sh

--- script/cours/zabbix.sh
+++ script/cours/zabbix.sh
@@ -17,11 +17,11 @@ else
 fi
 apt update
 apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent -y
-echo "create database IF NOT EXISTS zabbix character set utf8mb4 collate utf8mb4_bin;" > zbx.sql
-echo "create user IF NOT EXISTS zabbix@localhost identified by 'zabbix';" >> zbx.sql
-echo "grant all privileges on zabbix.* to zabbix@localhost;" >> zbx.sql
-echo "set global log_bin_trust_function_creators = 1;" >> zbx.sql
-echo "flush privileges;" >> zbx.sql
+echo "create database IF NOT EXISTS zabbix character set utf8mb4 collate utf8mb4_bin;
+create user IF NOT EXISTS zabbix@localhost identified by 'zabbix';
+grant all privileges on zabbix.* to zabbix@localhost;
+set global log_bin_trust_function_creators = 1;
+flush privileges;" > zbx.sql
 mysql -uroot -p$pass < zbx.sql
 zcat /usr/share/zabbix/sql-scripts/mysql/server.sql.gz > zbx.sql
 echo "set global log_bin_trust_function_creators = 0;" >> zbx.sql
