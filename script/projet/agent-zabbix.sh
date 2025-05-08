95f1f4ab68d0727454c5c68af6c39116f55dbcb5
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-05-08 11:32:55 +0200

    Update agent-zabbix.sh

--- script/projet/agent-zabbix.sh
+++ script/projet/agent-zabbix.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-read -p "quel distribution linux avez vous ? 1 pour debian 2 pour ubuntu" linux
+read -p "quel distribution linux avez vous ? 1 pour debian 2 pour ubuntu ? " linux
 if [[ $linux == 1 ]]; then
   wget https://repo.zabbix.com/zabbix/7.2/release/debian/pool/main/z/zabbix-release/zabbix-release_latest_7.2+debian12_all.deb
   dpkg -i zabbix-release_latest_7.2+debian12_all.deb
 elif [[ $linux == 2 ]]; then
-  wget https://repo.zabbix.com/zabbix/7.2/release/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.2+ubuntu24.04_all.deb
-  dpkg -i zabbix-release_latest_7.2+ubuntu24.04_all.deb
+  wget https://repo.zabbix.com/zabbix/7.2/release/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.2+ubuntu22.04_all.deb
+  dpkg -i zabbix-release_latest_7.2+ubuntu22.04_all.deb
 else
   echo "choix incorrect seuls debian et ubuntu sont pris en compte"
 fi
