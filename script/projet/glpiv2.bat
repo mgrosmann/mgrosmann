03ae5a505eb7fa190f72d0f6068ff164e81f9011
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-05-09 15:49:24 +0200

    Update glpiv2.bat

--- script/projet/glpiv2.bat
+++ script/projet/glpiv2.bat
@@ -1,7 +1,2 @@
-@echo off
-if exist "C:\Program Files\GLPI-Agent\" (
-  exit
-)
-path = \\192.168.1.10\script$\glpi.msi
-msiexec /i %path% /quiet RUNNOW=1 SERVER=http://192.168.1.11/glpi/front/inventory.php
+msiexec /i “\\192.168.1.10\script$\glpi.msi“ /quiet RUNNOW=1 SERVER=http://192.168.1.11/glpi/front/inventory.php
 curl http://localhost:62354/now
